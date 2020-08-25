1bad83a691f6d082848c05f4551d7af61c8c8e51
jahube <70081790+jahube@users.noreply.github.com>
2020-08-25 15:11:39 +0200

    Update OWA-AutoCompleteCache.ps1

--- OWA-AutoCompleteCache.ps1
+++ OWA-AutoCompleteCache.ps1
@@ -6,4 +6,5 @@ $mbxs = get-mailbox -ResultSize unlimited; $count= $MBX.count
 for ($M = 0; $M -lt $MBX.count; $M++) { $S =" [MBX] ($($M+1)/$count)  [Time]"
 $A = "Clearing OWA Autocomplete Cache [Mailbox Count] ($($M+1)/$count) [Mailbox] $($MBX[$M])"
 Write-Progress -Activity $A -Status $S -PercentComplete (($M/$count)*100) -SecondsRemaining $($count-$M) ;
-Try { Set-MailboxRegionalConfiguration @param } catch { Write-Host $Error[0].Exception.Message -F Yellow } }
+Try { Remove-MailboxUserConfiguration -Mailbox $MBX[$M] -Identity $c -Confirm:$false 
+} catch { Write-Host $Error[0].Exception.Message -F Yellow } }
