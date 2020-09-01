dd11e3fbc1931f1e92fcb77b62b5f0c9675361a3
jahube <70081790+jahube@users.noreply.github.com>
2020-09-02 01:03:55 +0200

    Create PartnerApplication-Short.ps1

---
+++ PartnerApplication/PartnerApplication-Short.ps1
@@ -0,0 +1,8 @@
+﻿# if not successful refer to the long versions in parent folder
+
+$exch = [Microsoft.Exchange.Data.Directory.SystemConfiguration.WellknownPartnerApplicationIdentifiers]::Exchange;
+$exchApp = Get-PartnerApplication $exch
+$exchAppName = “Exchange Online”
+$appAccountName = $exchApp.Name + “-ApplicationAccount”;
+$appAccount = Get-LinkedUser -Identity $appAccountName –ErrorAction
+Set-PartnerApplication -Identity $exchApp.Identity -LinkedAccount $appAccount.Identity
