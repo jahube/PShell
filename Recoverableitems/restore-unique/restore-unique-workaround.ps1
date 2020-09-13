ffebf6ee48684f6eff9e5b1095ae4a6a587e349d
jahube <70081790+jahube@users.noreply.github.com>
2020-09-13 15:34:12 +0200

    Create restore-unique-workaround.ps1

---
+++ Recoverableitems/restore-unique/restore-unique-workaround.ps1
@@ -0,0 +1,40 @@
+﻿##############
+#   search   #
+##############
+$user = 'admin@edu.dnsabr.com'
+
+$search = Get-RecoverableItems $user -FilterStartTime (get-date).AddDays(-180) -FilterEndTime (get-date).Addyears(2)
+
+$Filter = $search | Sort-Object | Select-Object subject -unique
+
+Write-host "`nFilter $($filter.count)" -F green
+#################################################################################
+#  matching filtered with first entry (adding back entryID from first match [0] #
+#################################################################################
+# $new = $null
+$new = @()
+
+foreach ($F in $filter) {
+
+$unique = ($search | where { $_.subject -eq $f.Subject })[-1]
+
+$new += $unique
+}
+$new  | Select subject,entryid
+Write-host "`nFilter + entryID $($new.count)" -F green
+##############
+#  restoring #
+##############
+$done = @()
+$failed = @()
+Foreach ($n in $new) {
+try { Restore-RecoverableItems -Identity $user -EntryID "$($n.entryid)" -EA 'stop' ;
+$done += $n
+} catch { 
+Write-Host "$($n.subject) failed to restore" -F yellow ;
+$failed += $n
+        }  
+}
+##############
+Write-host "`nrestored $($done.count)" -F green
+Write-host "`nFailed $($failed.count)" -F yellow
