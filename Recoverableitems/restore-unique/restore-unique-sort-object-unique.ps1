8e2e1197cfe6cbc7074040dfa20e19fd8b164496
jahube <70081790+jahube@users.noreply.github.com>
2020-09-13 15:34:08 +0200

    Create restore-unique-sort-object-unique.ps1

---
+++ Recoverableitems/restore-unique/restore-unique-sort-object-unique.ps1
@@ -0,0 +1,30 @@
+﻿##############
+#   search   #
+##############
+$user = 'affected@user.com'
+
+$search = Get-RecoverableItems $user -FilterStartTime (get-date).AddDays(-180) -FilterEndTime (get-date).Addyears(2)
+
+$unique = $search | Sort-Object subject -unique
+$search | Sort-Object EntryID | Select subject,EntryID
+$unique | Select subject,EntryID
+
+Write-host "`nFound $($search.count)" -F yellow
+Write-host "`nUnique $($unique.count)" -F green
+
+##############
+#  restoring #
+##############
+$done = @()
+$failed = @()
+Foreach ($n in $unique) {
+try { Restore-RecoverableItems -Identity $user -EntryID "$($n.entryid)" -EA 'stop' ;
+$done += $n
+} catch { 
+Write-Host "$($n.subject) failed to restore" -F yellow ;
+$failed += $n
+        }  
+}
+##############
+Write-host "`nrestored $($done.count)" -F green
+Write-host "`nFailed $($failed.count)" -F yellow
