8ba9379518cbc77abca8fd9fd713636cafc902e1
jahube <70081790+jahube@users.noreply.github.com>
2020-09-16 18:04:34 +0200

    Update OWA-AutoCompleteCache.ps1

--- OWA-AutocompleteCache/OWA-AutoCompleteCache.ps1
+++ OWA-AutocompleteCache/OWA-AutoCompleteCache.ps1
@@ -8,3 +8,10 @@ $A = "Clearing OWA Autocomplete Cache [Mailbox Count] ($($M+1)/$count) [Mailbox]
 Write-Progress -Activity $A -Status $S -PercentComplete (($M/$count)*100) -SecondsRemaining $($count-$M) ;
 Try { Remove-MailboxUserConfiguration -Mailbox $MBX[$M] -Identity $c -Confirm:$false 
 } catch { Write-Host $Error[0].Exception.Message -F Yellow } }
+
+
+# Outlook Offline Addressbook - for reference
+# delete subfolders when Outlook is closed
+# the OAB will be immediately downloaded again once Outlook is started
+# Filemanager OR  Search beside Start menu
+# %LocalAppData%\Microsoft\Outlook\Offline Address Books\
