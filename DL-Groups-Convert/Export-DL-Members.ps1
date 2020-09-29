47bef49b16d8e706515874f8af7341850d7afb5f
jahube <70081790+jahube@users.noreply.github.com>
2020-09-29 15:26:39 +0200

    Create Export-DL-Members.ps1

---
+++ DL-Groups-Convert/Export-DL-Members.ps1
@@ -0,0 +1,21 @@
+﻿
+$path = "C:\temp"
+mkdir $path\short
+mkdir $path\complete
+$DLS = Get-DistributionGroup -ResultSize unlimited
+$C = 0
+
+$count = $DLS.count
+$Members = @()
+#check
+foreach ($DL in $DLS) {
+Write-Progress -Activity "Group $($DL.name) $($DL.userprincipalname)" -Status "Group $($C) / $($count)" -PercentComplete (($C/$count)*100) -SecondsRemaining "$($count-$C)" ;
+$Members = @()
+$ps = $path + '\short\' + $DL.PrimarySmtpAddress + '.csv'
+$pc = $path + '\complete\' + $DL.PrimarySmtpAddress + '.csv'
+$Members = Get-DistributionGroupMember –Identity $DL.distinguishedname –ResultSize Unlimited
+$Members | Export-Csv -Path $pc -NoTypeInformation -Encoding UTF8
+$Members | select *name*,alias,pri*,GUID | Export-Csv -Path $ps -NoTypeInformation -Encoding UTF8
+$C++
+
+}
