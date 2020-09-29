acd81c34921086a98f79d5aa1b950e59f71211a6
jahube <70081790+jahube@users.noreply.github.com>
2020-09-29 15:29:55 +0200

    Create tickle-mailcontacts.ps1

---
+++ Addressbook-GAL-tickling-AutocompleteCache/tickle-mailcontacts.ps1
@@ -0,0 +1,8 @@
+﻿$MailContacts = get-MailContact -resultsize unlimited
+Write-Host
+Write-Host “Mail Contacts Found:” $count
+foreach($mailcontact in $mailcontacts){
+$i++
+Set-MailContact $mailcontact.alias -SimpleDisplayName $mailcontact.SimpleDisplayName -WarningAction silentlyContinue
+Write-Progress -Activity “Tickling Mail Contact [$count]…” -Status $i
+}
