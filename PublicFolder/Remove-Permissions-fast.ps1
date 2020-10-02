5bc97a419640a12b1ac86e7784a21a70b6c48a38
jahube <70081790+jahube@users.noreply.github.com>
2020-10-02 11:51:18 +0200

    Create Remove-Permissions-fast.ps1

---
+++ PublicFolder/Remove-Permissions-fast.ps1
@@ -0,0 +1,20 @@
+﻿$folderscope = '\'
+
+$success = New-Object System.Collections.ArrayList
+$errors = New-Object System.Collections.ArrayList
+$PFs = get-publicfolder $folderscope –Recurse
+#$PS = $PFs | Get-PublicFolderClientPermission
+#$PS = $PS | ? { ($_.user.displayname -notmatch "Default") -and ($_.user.displayname -notmatch "Anonymous") }
+$count = $PFs.count
+for ($P = 0; $P -lt $PFs.count; $P++) { 
+$F = $PFs[$P].identity.ToString()
+#$DN = $PS[$P].user.displayname.ToString()
+#$DST = $PS[$P].user.RecipientPrincipal
+# $AR = $PS[$P].accessrights
+#$SPF = $PS[$P].SharingPermissionFlags
+Write-Progress -Activity "Remove permission - [current Folder] $($F)" -Id 2 -ParentId 1 -Status "For [USER] $($DN)" -PercentComplete (($P/$count)*100) -SecondsRemaining $($count-$P) ;
+
+Get-PublicFolderClientPermission -Identity $F | Remove-PublicFolderClientPermission -ErrorAction silentlycontinue
+}
+
+# get-publicfolder \ -Recurse | Get-PublicFolderClientPermission | Remove-PublicFolderClientPermission
