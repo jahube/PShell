81f1fcc8fe6355ed01ebc63bd15bb3498c554d1b
jahube <70081790+jahube@users.noreply.github.com>
2020-10-19 10:16:54 +0200

    Create Remove-Permissions-entryid.ps1

---
+++ PublicFolder/Remove-Permissions-entryid.ps1
@@ -0,0 +1,15 @@
+$folderscope = '\'
+$empty = 0
+$PFs = get-publicfolder $folderscope –Recurse -ResultSize unlimited
+[System.Collections.ArrayList]$PFE = ($PFs).entryid
+$count = $PFE.count
+for ($F = 0; $F -lt $PFE.count; $F++) { 
+Write-Progress -Activity "Remove permission" -Id 2 -ParentId 1 -Status "Remove permission: $([math]::Round($(($F/$count)*100))) %" -PercentComplete (($F/$count)*100) -SecondsRemaining $($count-$F)
+$PS = Get-PublicFolderClientPermission -identity $PFE[$F]
+[System.Collections.ArrayList]$PM = $PS.user.RecipientPrincipal.LegacyExchangeDN
+if ($PM) { 
+foreach ($U in $PM) { 
+#try{ 
+Remove-PublicFolderClientPermission -Identity $PFE[$F] -User $U -Confirm:$false
+# -EA 'stop' } catch {  Write-host $Error[0].Exception.Message -F yellow }
+ } } else { $empty++ } } # end
