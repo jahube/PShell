f5e3cb72230ad946b7086f33475c32107e4f5abc
jahube <70081790+jahube@users.noreply.github.com>
2020-11-19 01:11:28 +0200

    Update Remove-Permissions-entryid.ps1

--- PublicFolder/Remove-Permissions-entryid.ps1
+++ PublicFolder/Remove-Permissions-entryid.ps1
@@ -6,7 +6,7 @@ $count = $PFE.count
 for ($F = 0; $F -lt $PFE.count; $F++) { 
 Write-Progress -Activity "Remove permission" -Id 2 -ParentId 1 -Status "Remove permission: $([math]::Round($(($F/$count)*100))) %" -PercentComplete (($F/$count)*100) -SecondsRemaining $($count-$F)
 $PS = Get-PublicFolderClientPermission -identity $PFE[$F]
-[System.Collections.ArrayList]$PM = $PS.user.RecipientPrincipal.LegacyExchangeDN
+[System.Collections.ArrayList]$PM = @($PS.user.RecipientPrincipal.LegacyExchangeDN)
 if ($PM) { 
 foreach ($U in $PM) { 
 #try{ 
