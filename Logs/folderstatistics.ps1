557f5d25c62aa2b1760b931e1a49839760ec40be
jahube <70081790+jahube@users.noreply.github.com>
2020-11-20 11:43:59 +0200

    Create folderstatistics.ps1

---
+++ Logs/folderstatistics.ps1
@@ -0,0 +1,21 @@
+$ts = Get-Date -Format yyyyMMdd_hhmmss ; $FormatEnumerationLimit = -1
+$DesktopPath = ([Environment]::GetFolderPath('Desktop'))
+$logsPATH =mkdir "$DesktopPath\MS-Logs\$ts"
+Start-Transcript "$logsPATH\Folderstatistics$ts.txt" -Verbose
+$mbx = "affected user"
+get-mailboxfolderstatistics $mbx | export-clixml $logsPATH\EXO_IPM_MbxFStats.xml
+get-mailboxfolderstatistics $mbx -FolderScope nonIPMroot | export-clixml $logsPATH\EXO_NONIPM_MbxFStats.xml
+get-mailboxfolderstatistics $mbx -FolderScope all | export-clixml $logsPATH\EXO_all_MbxFStats.xml
+get-mailboxfolderstatistics $mbx | ft folderpath,containerclass,foldertype,itemsinfolder,itemsinfolderandsubfolders
+get-mailboxfolderstatistics $mbx | ft folderpath,containerclass,foldertype,itemsinfolder,itemsinfolderandsubfolders > $logsPATH\list.txt
+get-mailboxfolderstatistics $mbx -FolderScope nonIPMroot | ft folderpath, itemsinfolder, itemsinfolderandsubfolders
+get-mailboxfolderstatistics $mbx -FolderScope all | ft folderpath, itemsinfolder, itemsinfolderandsubfolders
+Get-MailboxFolderstatistics $mbx | Select-Object *,@{ Name = "SizeInBytes"; Expression={ [Uint64]::Parse([Regex]::Match($_.FolderSize.ToString(), '\(([0-9,]+) bytes\)').Groups[1].Value.Replace(",","")) } } | Sort SizeInBytes -Descending | ft FolderPath,FolderSize,itemsinfolder
+Get-MailboxFolderstatistics $mbx -FolderScope nonIPMroot | Select-Object *,@{ Name = "SizeInBytes"; Expression={ [Uint64]::Parse([Regex]::Match($_.FolderSize.ToString(), '\(([0-9,]+) bytes\)').Groups[1].Value.Replace(",","")) } } | Sort SizeInBytes -Descending | ft FolderPath,FolderSize,itemsinfolder
+Get-MailboxFolderstatistics $mbx -FolderScope all | Select-Object *,@{ Name = "SizeInBytes"; Expression={ [Uint64]::Parse([Regex]::Match($_.FolderSize.ToString(), '\(([0-9,]+) bytes\)').Groups[1].Value.Replace(",","")) } } | Sort SizeInBytes -Descending | ft FolderPath,FolderSize,itemsinfolder
+######END SCRIPT ##########################
+Stop-Transcript
+$destination = "$DesktopPath\MS-Logs\Logs_$ts.zip"
+Add-Type -assembly “system.io.compression.filesystem”
+[io.compression.zipfile]::CreateFromDirectory($logsPATH, $destination) # ZIP
+Invoke-Item $DesktopPath\MS-Logs # open file manager
