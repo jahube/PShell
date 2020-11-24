f3ed8515b073206b394ab6b4ce91f58e536d306e
jahube <70081790+jahube@users.noreply.github.com>
2020-11-24 15:39:05 +0200

    Update mailboxrestorerequest.ps1

--- Misc/mailboxrestorerequest.ps1
+++ Misc/mailboxrestorerequest.ps1
@@ -1,37 +1,36 @@
-﻿wget https://aka.ms/hybridconnectivity -OutFile c:\tm.psm1
-Import-Module c:\tm.psm1
-Test-HybridConnectivity -TestO365Endpoints
-get-mailbox -SoftDeletedMailbox | Get-MailboxFolderStatistics -IncludeSoftDeletedRecipients | fl *items*
+﻿#statistics
 
-get-mailbox 8f8eb835-c48c-41e1-ad88-c869a0dc920e | fl sam*
-Get-Mailbox Jenn58241-1509788987
+get-mailbox -SoftDeletedMailbox | Get-MailboxFolderStatistics -IncludeSoftDeletedRecipients | ft *name*,*items*
 
+# old syntax
 
-
-
-$source = "Abagael.C.Nestor@edu.dnsabr.com"
-$target = "JenniferB@edu.dnsabr.com"
-
-$MBX = Get-Mailbox -SoftDeletedMailbox $source
-$TGT = Get-Mailbox $target
-
-New-MailboxRestoreRequest -SourceMailbox $MBX.SamAccountName -TargetMailbox $TGT.SamAccountName -AllowLegacyDNMismatch
-
-Aaron.A.Pauley   
-
-$source = "Aaron.A.Pauley@edu.dnsabr.com"
-$target = "HenryM@edu.dnsabr.com"
+$source = "SOURCE@DOMAIN.com"
+$target = "TARGET@DOMAIN.com"
 
 $MBX = (Get-Mailbox -SoftDeletedMailbox $source).DistinguishedName
 $TGT = (Get-Mailbox $target).DistinguishedName
 New-MailboxRestoreRequest -SourceMailbox $MBX.DistinguishedName -TargetMailbox $TGT.SamAccountName -AllowLegacyDNMismatch
 
-$source = "Aaron.A.Pauley@edu.dnsabr.com"
-$target = "HenryM@edu.dnsabr.com"
+# new syntax
+
+$source = "SOURCE@DOMAIN.com"
+$target = "TARGET@DOMAIN.com"
 $param = @{ AllowLegacyDNMismatch = $true
 SourceMailbox = (Get-Mailbox -SoftDeletedMailbox $source).DistinguishedName
 TargetMailbox = (Get-Mailbox $target).DistinguishedName }
+
 New-MailboxRestoreRequest @param
 
-get-MailboxRestoreRequest | remove-MailboxRestoreRequest
-Get-InboxRule -Mailbox admin@edu.dnsabr.com
+#status
+get-MailboxRestoreRequest
+
+#To check the progress of the restore:
+
+Get-MailboxRestoreRequest ThereseRestore | Get-MailboxRestoreRequestStatistics | fl
+
+#To troubleshoot:
+
+Get-MailboxRestoreRequest ThereseRestore | Get-MailboxRestoreRequestStatistics -IncludeReport | fl
+
+# to remove failed request
+# get-MailboxRestoreRequest | remove-MailboxRestoreRequest
