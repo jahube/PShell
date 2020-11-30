3d68eeed9bf411d28dff537f3448a43936b0f426
jahube <70081790+jahube@users.noreply.github.com>
2020-11-30 11:58:08 +0200

    Update Hybrid-Delegation.ps1

--- Hybrid-Delegation/Hybrid-Delegation.ps1
+++ Hybrid-Delegation/Hybrid-Delegation.ps1
@@ -1,47 +1,53 @@
-•	Automapping
+FULL ACCESS
+
+• full access (MailboxPermission)
+
+Add-MailboxPermission -Identity "Terry Adams" -User "Kevin Kelly" -AccessRights FullAccess -InheritanceType All
+
+• Automapping (msExchDelegateListLink)
+
 $A = Get-ADUser a3
-
 $b = Get-ADUser TEST
-
 $dn = $b.DistinguishedName
-
 Set-ADUser -Identity $A.DistinguishedName -Add @{msExchDelegateListLink=$dn}
-
 get-ADUser -Identity $A.DistinguishedName -Properties msExchDelegateListLink | select -ExpandProperty msExchDelegateListLink
 
 
+ACLABLE 
+
 Set-OrganizationConfig -ACLableSyncedObjectEnabled $True
 
-•	update mailboxes
+•	update mailboxes (ACLable user MBX onprem)
 
-$mbxs = get-mailbox -ResultSize unlimited
-
+$mbxs = get-mailbox -ResultSize unlimited | ? { $_.recipienttypedetails -eq "UserMailbox" }
 Foreach ($M in $mbxs) { Set-ADUser $M.distinguishedname -Replace @{msExchRecipientDisplayType=-1073741818} }
 
-•	update remotemailboxes
+•	update remotemailboxes (ACLable remote MBX cloud)
 
-$RmtMbx = get-remotemailbox -ResultSize unlimited
-
+$RmtMbx = get-remotemailbox -ResultSize unlimited | ? { $_.recipienttypedetails -eq 'RemoteUserMailbox' } 
 Foreach ($R in $RmtMbx) { Set-ADUser $R.distinguishedname -Replace @{msExchRecipientDisplayType=-1073740282} }
 
-# ACCESS
+
+# SENDAS ACCESS
+
 ## pending doublecheck to not mix up sendonbehalf + sendas / remotemailbox
 # Local --> Cloud has to be set from Onprem
 # Cloud --> Local has to be set from Cloud
 
-#onprem
+#onprem "Send As"
 $MBX = "accessed@mailbox" $user = "accessing@USER"
 $M = get-aduser -filter { userprincipalname -eq $MBX }
 $U = get-aduser -filter { userprincipalname -eq $user}
 Add-ADPermission -Identity $M.distinguishedname -User $U.distinguishedname -AccessRights ExtendedRight -ExtendedRights "Send As"
 
-#cloud
+#cloud SendAs
 $MBX = "accessed@mailbox"
 $user = "accessing@USER"
 
 Add-RecipientPermission -Identity $MBX -Trustee $user -AccessRights SendAs -Confirm:$false
 
-# ACCESS 
+
+# ON BEHALF ACCESS (outlook) 
 ## pending doublecheck to not mix up sendonbehalf + sendas / remotemailbox
 
 # Local --> Cloud has to be set from Onprem
@@ -51,5 +57,5 @@ cloud > onprem mailuser
 set-mailbox MBX-grantonbehalfto USER
 
 onprem remotemailbox > onpem mailbox
-set-remotemailbox MBX-grantonbehalfto USER
+set-remotemailbox MBX -grantonbehalfto USER
 
