38bb83ebba94f7d81ec618a33c7868f743b04033
jahube <70081790+jahube@users.noreply.github.com>
2020-11-30 14:31:18 +0200

    Update Trackinglog.ps1

--- Trackinglog/Trackinglog.ps1
+++ Trackinglog/Trackinglog.ps1
@@ -1,9 +1,16 @@
-Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -sender “affected@sender.com" | Export-CsV C:\Temp\sendertrackinglog1.csv
 
-Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -sender “affected@recipient.com" | Export-CsV C:\Temp\receivetrackinglog1.csv 
+# Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -sender “affected@sender.com" | Export-CsV C:\Temp\sendertrackinglog1.csv
 
-# if it has any issues, then without the pipeline
+# Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -sender “affected@recipient.com" | Export-CsV C:\Temp\receivetrackinglog1.csv 
+
+Start-transcript
 
 Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -sender “affected@sender.com" | Export-CsV C:\Temp\sendertrackinglog2.csv
 
 Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -Recipients “affected@recipient.com" | Export-CsV C:\Temp\receivetrackinglog2.csv
+
+Get-SendConnector |fl
+
+Get-RemoteDomain |fl
+
+Stop-transcript
