a4609b045b92f31116aa43618a26f7ebc40cadf1
jahube <70081790+jahube@users.noreply.github.com>
2020-12-01 18:39:36 +0200

    Create permissions.ps1

---
+++ permissions/permissions.ps1
@@ -0,0 +1,19 @@
+# https://protection.office.com/permissions eDiscovery Admin
+
+install-module exchangeonlinemanagement
+
+Connect-ExchangeOnline
+
+$admin = 'admin@DOMAIN.com'
+
+Add-RoleGroupMember -Identity eDiscoveryManager -Member $admin
+ 
+install-module MSOnline
+
+Connect-MsolService
+
+$obj = $((get-MsolRole -RoleName 'Compliance Administrator').ObjectID.guid)
+
+$adm = $((Get-MsolUser -UserPrincipalName $admin).ObjectID.guid)
+
+add-MsolRoleMember -RoleObjectId $obj -RoleMemberObjectId $adm
