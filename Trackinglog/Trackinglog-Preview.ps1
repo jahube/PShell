ab5bf78753ae48db8e513e05f3920a2b64705875
jahube <70081790+jahube@users.noreply.github.com>
2020-12-01 07:06:48 +0200

    Update Trackinglog-Preview.ps1

--- Trackinglog/Trackinglog-Preview.ps1
+++ Trackinglog/Trackinglog-Preview.ps1
@@ -15,7 +15,8 @@ $logsPATH =mkdir "$DesktopPath\MS-Logs\ConnectorLogs_$ts" # creates MS-Logs on d
 ########  Start Transcipt  ##########
 
 Start-Transcript "$logsPATH\ConnectorLogs_$ts.txt" -Verbose
-
+Get-SendConnector | fl > "$logsPATH\SendConnector-FL-before.txt"
+Get-ReceiveConnector | fl > "$logsPATH\ReceiveConnector-FL-before.txt"
 Get-RemoteDomain |ft DomainName,IsInternal,TargetDeliveryDomain,TrustedMail*,OriginatingServer > "$logsPATH\RemoteDomain-before.txt"
 
 Get-ExchangeCertificate | Format-List FriendlyName,Subject,CertificateDomains,Thumbprint,Services > "$logsPATH\certificates.txt"
@@ -56,8 +57,8 @@ Get-RemoteDomain |ft DomainName,IsInternal,TargetDeliveryDomain,TrustedMail*,Ori
 Get-TransportService | icm { Get-MessageTrackingLog -Start (get-date).AddDays($timeframe) -End (get-date) -sender $sender | Export-CsV $logsPATH\sendertrackinglog.csv  }
 Get-TransportService | icm { Get-MessageTrackingLog -Start (get-date).AddDays($timeframe) -End (get-date) -Recipients $recipient | Export-CsV $logsPATH\receivetrackinglog.csv  }
 
-Get-SendConnector | fl > "$logsPATH\SendConnector-FL.txt"
-Get-ReceiveConnector | fl > "$logsPATH\ReceiveConnector-FL.txt"
+Get-SendConnector | fl > "$logsPATH\SendConnector-FL-After.txt"
+Get-ReceiveConnector | fl > "$logsPATH\ReceiveConnector-FL-After.txt"
 Get-AuthConfig | fl > "$logsPATH\AuthConfig-FL.txt"
 
 Foreach ($i in (Get-ExchangeServer)) {Write-Host $i.FQDN; icm { Get-ExchangeCertificate -Server $i.Identity } }
