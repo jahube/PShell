414aa2074a34fb2f230766fcc932b16d05cafa93
jahube <70081790+jahube@users.noreply.github.com>
2020-12-01 06:49:57 +0200

    Update Trackinglog.ps1

--- Trackinglog/Trackinglog.ps1
+++ Trackinglog/Trackinglog.ps1
@@ -1,5 +1,12 @@
+
+
+Get-ReceiveConnector | Set-ReceiveConnector -ProtocolLoggingLevel verbose
+Get-SendConnector | Set-SendConnector -ProtocolLoggingLevel verbose
+
 $sender = “affected@sender.com"
 $recipient = “affected@recipient.com"
+$MessageID = "<MessageID>"
+$timeframe = “-5" # ("days back")
 
 $VerbosePreference = 'Continue'
 $ts = Get-Date -Format yyyyMMdd_hhmmss
@@ -8,9 +15,13 @@ $DesktopPath = ([Environment]::GetFolderPath('Desktop'))
 $logsPATH =mkdir "$DesktopPath\MS-Logs\$ts" # creates MS-Logs on desktop + Timestamp
 Start-Transcript "$logsPATH\OnPremises$ts.txt" -Verbose
 
-Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -sender “affected@sender.com" | Export-CsV $logsPATH\sendertrackinglog2.csv
+Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays($timeframe) -End (get-date) -sender “affected@sender.com" | Export-CsV $logsPATH\Sender-trackinglog.csv
 
-Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays(-15) -End (get-date) -Recipients “affected@recipient.com" | Export-CsV $logsPATH\receivetrackinglog2.csv
+Get-TransportService | Get-MessageTrackingLog -Start (get-date).AddDays($timeframe) -End (get-date) -Recipients “affected@recipient.com" | Export-CsV $logsPATH\Receive-trackinglog.csv
+
+$Servers = Get-ExchangeServer;  $Servers | where {​​$_.isHubTransportServer -eq $true -or $_.isMailboxServer -eq $true}​​ | Get-MessageTrackingLog -MessageId $MessageID  | | Export-CsV $logsPATH\MessageID-trackinglog.csv
+
+Get-queue ; (Get-queue).LastError
 
 Get-RemoteDomain |ft DomainName,IsInternal,TargetDeliveryDomain,TrustedMail*,OriginatingServer
 Get-RemoteDomain |fl
