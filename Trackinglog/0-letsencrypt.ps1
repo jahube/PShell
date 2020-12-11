5952299a768b6f43cf031d3ebb103b57ad52319b
jahube <70081790+jahube@users.noreply.github.com>
2020-12-11 14:45:16 +0200

    Delete 0-letsencrypt.ps1

--- Trackinglog/0-letsencrypt.ps1
+++
@@ -1,5 +0,0 @@
-﻿Install-Module -Name Posh-ACME
- 
-$certNames = 'domain.com','*.domain.com'
- 
-New-PACertificate $certNames -AcceptTOS -Contact admin@domain.com -DnsPlugin AcmeDns -PluginArgs @{ACMEServer='auth.acme-dns.io'} -Install -FriendlyName 'domain.com' -PreferredChain "ISRG Root X2" -Force
