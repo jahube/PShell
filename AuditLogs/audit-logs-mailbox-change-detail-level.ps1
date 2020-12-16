30b40bd6d887c618f12301fcb9cb9fba081f07ba
jahube <70081790+jahube@users.noreply.github.com>
2020-12-16 12:10:36 +0200

    Update audit-logs-mailbox-change-detail-level.ps1

--- AuditLogs/audit-logs-mailbox-change-detail-level.ps1
+++ AuditLogs/audit-logs-mailbox-change-detail-level.ps1
@@ -1,28 +1,33 @@
-
 start-transcript -verbose
 
 $user = "AFFECTED@USER.com"  # please modify
 
+# check results BEFORE
 $PROW1 = Get-Mailbox $user | select -expandproperty AuditOwner
  Write-host "BEFORE: AuditOwner = $($PROW1.count)" -foregroundcolor yellow;  Write-host "AuditOwner: $($PROW1)" -foregroundcolor Cyan
 $PRDL1 =Get-Mailbox $user | select -expandproperty AuditDelegate
  Write-host "BEFORE: AuditOwner = $($PRDL1.count)" -foregroundcolor yellow;  Write-host "AuditOwner: $($PRDL1)" -foregroundcolor Cyan
  
-Get-Mailbox $user | Set-Mailbox -AuditEnabled $true `
--AuditOwner AddFolderPermissions, ApplyRecord, Create, Send, HardDelete, MailboxLogin, ModifyFolderPermissions, Move, MoveToDeletedItems, RecordDelete, RemoveFolderPermissions, SoftDelete, Update, UpdateFolderPermissions, UpdateCalendarDelegation, UpdateInboxRules `
--AuditDelegate AddFolderPermissions, ApplyRecord, Create, FolderBind, HardDelete, ModifyFolderPermissions, Move, MoveToDeletedItems, RecordDelete, RemoveFolderPermissions, SendAs, SendOnBehalf, SoftDelete, Update, UpdateFolderPermissions, UpdateInboxRules `
--AuditAdmin Copy, Create, HardDelete, MoveToDeletedItems, RecordDelete, RemoveFolderPermissions, SendAs, SendOnBehalf, SoftDelete, Update, UpdateFolderPermissions, UpdateCalendarDelegation, UpdateInboxRules
+# Apply ALL DETAILS
+$Parameter = @{ identity = $user ; AuditEnabled = $true ;
+AuditOwner = 'AddFolderPermissions', 'ApplyRecord', 'Create', 'Send', 'HardDelete', 'MailboxLogin', 'ModifyFolderPermissions', 'Move', 'MoveToDeletedItems', 'RecordDelete', 'RemoveFolderPermissions', 'SoftDelete', 'Update', 'UpdateFolderPermissions', 'UpdateCalendarDelegation', 'UpdateInboxRules' ;
+AuditDelegate = 'AddFolderPermissions', 'ApplyRecord', 'Create', 'FolderBind', 'HardDelete', 'ModifyFolderPermissions', 'Move', 'MoveToDeletedItems', 'RecordDelete', 'RemoveFolderPermissions', 'SendAs', 'SendOnBehalf', 'SoftDelete', 'Update', 'UpdateFolderPermissions', 'UpdateInboxRules' ;
+AuditAdmin = 'Copy', 'Create', 'HardDelete', 'MoveToDeletedItems', 'RecordDelete', 'RemoveFolderPermissions', 'SendAs', 'SendOnBehalf', 'SoftDelete', 'Update', 'UpdateFolderPermissions', 'UpdateCalendarDelegation', 'UpdateInboxRules' }
+Set-Mailbox @Parameter 
 
-#OFF
-set-MailboxAuditBypassAssociation -Identity $user -AuditBypassEnabled $true
+# On /Off to refresh update
+set-MailboxAuditBypassAssociation -Identity $user -AuditBypassEnabled $true  #OFF
+set-MailboxAuditBypassAssociation -Identity $user -AuditBypassEnabled $false  #ON
 
-#ON
-set-MailboxAuditBypassAssociation -Identity $user -AuditBypassEnabled $false 
-
-
+# recheck results AFTER
 $PROW = Get-Mailbox $user | select -expandproperty AuditOwner
  Write-host "AFTER: AuditOwner = $($PROW.count)" -foregroundcolor yellow;  Write-host "AuditOwner: $($PROW)" -foregroundcolor Cyan
 $PRDL =Get-Mailbox $user | select -expandproperty AuditDelegate
  Write-host "AFTER: AuditOwner = $($PRDL.count)" -foregroundcolor yellow;  Write-host "AuditOwner: $($PRDL)" -foregroundcolor Cyan
+
+# enable Unified Audit logs
+IF(!((Get-AdminAuditLogConfig).UnifiedAuditLogIngestionEnabled)) {
+Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true ;
+Write-host "Unified Audit log was disabled - ENABLING NOW" -F yellow }
 
 stop-transcript
