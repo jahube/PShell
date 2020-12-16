647a8f843d2b6afaa2c54ce8a989d66808058ff8
jahube <70081790+jahube@users.noreply.github.com>
2020-12-16 21:13:24 +0200

    Update Remove-Permissions-entryid.ps1

--- PublicFolder/Remove-Permissions-entryid.ps1
+++ PublicFolder/Remove-Permissions-entryid.ps1
@@ -1,15 +1,10 @@
 $folderscope = '\'
-$empty = 0
-$PFs = get-publicfolder $folderscope –Recurse -ResultSize unlimited
-[System.Collections.ArrayList]$PFE = ($PFs).entryid
-$count = $PFE.count
-for ($F = 0; $F -lt $PFE.count; $F++) { 
-Write-Progress -Activity "Remove permission" -Id 2 -ParentId 1 -Status "Remove permission: $([math]::Round($(($F/$count)*100))) %" -PercentComplete (($F/$count)*100) -SecondsRemaining $($count-$F)
-$PS = Get-PublicFolderClientPermission -identity $PFE[$F]
-[System.Collections.ArrayList]$PM = @($PS.user.RecipientPrincipal.LegacyExchangeDN)
-if ($PM) { 
-foreach ($U in $PM) { 
-#try{ 
-Remove-PublicFolderClientPermission -Identity $PFE[$F] -User $U -Confirm:$false
-# -EA 'stop' } catch {  Write-host $Error[0].Exception.Message -F yellow }
- } } else { $empty++ } } # end
+
+$PFs = get-publicfolder $folderscope –Recurse -ResultSize unlimited ;
+[System.Collections.ArrayList]$PFE = ($PFs).entryid ; $count = $PFE.count ;
+for ($F = 0; $F -lt $PFE.count; $F++) { $ac = "Remove permission" ;
+$Sts = "Remove permission: $([math]::Round($(($F/$count)*100))) %"; $Pc = (($F/$count)*100); $sc =$($count-$F)
+Write-Progress -Activity $ac -Id 2 -ParentId 1 -Status $Sts -PercentComplete $Pc -SecondsRemaining = $sc
+$PS = Get-PublicFolderClientPermission -identity $PFE[$F] ;
+[System.Collections.ArrayList]$PM = @($PS.user.RecipientPrincipal.LegacyExchangeDN) ;
+if ($PM) { foreach ($U in $PM) { Remove-PublicFolderClientPermission -Identity $PFE[$F] -User $U -CF:$false } }
