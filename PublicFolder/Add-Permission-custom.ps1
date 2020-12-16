5c51faa3b54bba279396390dc7d538bed83d485e
jahube <70081790+jahube@users.noreply.github.com>
2020-12-16 21:02:24 +0200

    Update Add-Permission-custom.ps1

--- PublicFolder/Add-Permission-custom.ps1
+++ PublicFolder/Add-Permission-custom.ps1
@@ -19,9 +19,8 @@ $access = (Get-mailbox -Identity $user).Distinguishedname
 # $access = (Get-DistributionGroup $Group).Distinguishedname ;
 ################################################################
 $PFs = get-publicfolder $folderscope -Recurse -ResultSize unlimited -EA silentlycontinue ;
-[System.Collections.ArrayList]$PFE = ($PFs).entryid
-$count = $PFE.count
-for ($F = 0; $F -lt $PFE.count; $F++) { 
-Write-Progress -Activity "Add pemission ($permission) - current Folder" -Id 2 -ParentId 1 -Status $PFE[$F] -PercentComplete (($F/$count)*100) -SecondsRemaining (($count-$F)*3) ;
+[System.Collections.ArrayList]$PFE = ($PFs).entryid ; $count = $PFE.count
+for ($F = 0; $F -lt $PFE.count; $F++) { $A = "Add pemission ($permission) - current Folder" ;
+Write-Progress -Activity $A -Id 2 -ParentId 1 -Status $PFE[$F] -PercentComplete (($F/$count)*100) -SecondsRemaining (($count-$F)*3) ;
 Add-PublicFolderClientPermission -Identity $PFE[$F] -user $access -AccessRights $permission -Confirm:$false }
 ################################################################
