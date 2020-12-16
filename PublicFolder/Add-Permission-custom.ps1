c71dac39ec1d568420d361d5ac027d1dd9dce8d1
jahube <70081790+jahube@users.noreply.github.com>
2020-12-16 20:57:53 +0200

    Update Add-Permission-custom.ps1

--- PublicFolder/Add-Permission-custom.ps1
+++ PublicFolder/Add-Permission-custom.ps1
@@ -1,25 +1,27 @@
-$folderscope = '\' ;
-
-$permission = 'CreateItems','ReadItems','CreateSubfolders','FolderOwner','FolderContact','FolderVisible','EditOwnedItems','EditAllItems' ;
-# $permission = 'Reviewer'
-# $permission = 'Editor'
-# $permission = 'Owner'
-# security group
-
-$Group = 'security group displayname'
-$access = (Get-DistributionGroup $Group).Distinguishedname ;
-
-
-# user mailbox
-
-$user = "admin@domain.com"
-$access = (Get-mailbox -Identity $user).Distinguishedname ;
-
-
+################################################################
+#        Change the PATH - RECURSIVE (INCL SUBFOLDER)          #
+################################################################
+$folderscope = '\'                                             # **MODIFY**
+################################################################
+# CHOOSE PERMISSION:     Owner, Editor, Reviewer, Author       #
+################################################################
+$permission = 'Owner'                                          # **MODIFY**
+################################################################
+# ALTERNATIVE: CUSTOM LIST OF PERMISSION TAGS
+# $permission = 'CreateItems','ReadItems','CreateSubfolders','FolderOwner','FolderContact','FolderVisible','EditOwnedItems','EditAllItems' ;
+################################################################
+$user = "Accessing@Mailbox.com"                                # **MODIFY**
+################################################################
+$access = (Get-mailbox -Identity $user).Distinguishedname
+################################################################
+# security group ALTERNATIVE
+# $Group = 'security group displayname'
+# $access = (Get-DistributionGroup $Group).Distinguishedname ;
+################################################################
 $PFs = get-publicfolder $folderscope -Recurse -ResultSize unlimited -EA silentlycontinue ;
 [System.Collections.ArrayList]$PFE = ($PFs).entryid
 $count = $PFE.count
 for ($F = 0; $F -lt $PFE.count; $F++) { 
 Write-Progress -Activity "Add pemission ($permission) - current Folder" -Id 2 -ParentId 1 -Status $PFE[$F] -PercentComplete (($F/$count)*100) -SecondsRemaining (($count-$F)*3) ;
-Add-PublicFolderClientPermission -Identity $PFE[$F] -user $access -AccessRights $permission -Confirm:$false ;
-}
+Add-PublicFolderClientPermission -Identity $PFE[$F] -user $access -AccessRights $permission -Confirm:$false }
+################################################################
