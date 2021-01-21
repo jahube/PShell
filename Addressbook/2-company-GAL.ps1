bc4fc45980000bb39c26e4b7e8375491e3b9d90b
jahube <70081790+jahube@users.noreply.github.com>
2021-01-21 18:08:18 +0200

    Update 2-company-GAL.ps1

--- Addressbook/2-company-GAL.ps1
+++ Addressbook/2-company-GAL.ps1
@@ -16,7 +16,7 @@ New-AddressList -Name "Ressources $company" -RecipientFilter {(RecipientTypeDeta
 New-OfflineAddressBook -Name "Offline Addressbook $company" -AddressLists "Addresslist Internal $company","Addresslist External $company","Addresslist Groups $company","Room Lists $company","Rooms $company","Ressources $company"
 
 # Create Global Address Book INTERNAL only
-New-GlobalAddressList  -Name "Global Adressbook Internal $company" -RecipientFilter "((RecipientType -eq 'UserMailbox') -or (RecipientType -eq 'Mailuser') -or (RecipientTypeDetails -eq 'SharedMailbox')) -and (Company -eq $company)"
+New-GlobalAddressList  -Name "Global Adressbook Internal $company" -RecipientFilter { ((RecipientType -eq 'UserMailbox') -or (RecipientType -eq 'Mailuser') -or (RecipientTypeDetails -eq 'SharedMailbox')) -and (Company -eq $company)}
 
 # Create Address Book Policy
 New-AddressBookPolicy -Name "Adressbook Policy $company” -AddressLists "Addresslist Internal $company","Addresslist External $company","Addresslist Groups $company","Room Lists $company","Rooms $company","Ressources $company" -RoomList "\Room Lists $company" -OfflineAddressBook “\Offline Addressbook $company” -GlobalAddressList “\Global Adressbook Internal $company”
