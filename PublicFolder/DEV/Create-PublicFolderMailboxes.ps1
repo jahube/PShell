10fdd1302bd45af346adb40c2b591ead379e921e
jahube <70081790+jahube@users.noreply.github.com>
2021-01-25 06:22:02 +0200

    Create Create-PublicFolderMailboxes.ps1

---
+++ PublicFolder/DEV/Create-PublicFolderMailboxes.ps1
@@ -0,0 +1,16 @@
+﻿$PFcount = "30"
+$Prefix = "PF"
+
+$PFNameArray = @() ; for ($N = 1; $N -le $PFcount ; $N++) { 
+if ($N -le "9") { [String]$name = [String]$($Prefix + '0' + $N) }
+Else            { [String]$name = [String]$($Prefix + $N) }
+$PFNameArray += $name }
+
+$command = @'
+Param($P) ;
+for ($P = 0; $P -lt $PFNameArray.count; $P++) { new-Mailbox -PublicFolder -Name $PFNameArray[$P] }
+'@
+
+#$session = Get-PSSession -InstanceId (Get-OrganizationConfig).RunspaceId.Guid
+$myscriptblock = [scriptblock]::Create("$command")
+Invoke-Command -ScriptBlock $myscriptblock -ArgumentList $PFNameArray
