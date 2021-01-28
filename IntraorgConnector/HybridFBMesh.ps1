558694e72bd9553b7f05c4edd3d1c0e82c0c9757
jahube <70081790+jahube@users.noreply.github.com>
2021-01-28 19:37:53 +0200

    Update HybridFBMesh.ps1

--- IntraorgConnector/HybridFBMesh.ps1
+++ IntraorgConnector/HybridFBMesh.ps1
@@ -6,25 +6,20 @@ Contoso          Fabrikam
 
 (2)      Onprem    (4)
 
-#SHORT
 
 >>> CLOUD
-
 Get-FederationInformation -DomainName Contoso.onmicrosoft.com | New-OrganizationRelationship -Name "Contoso" -FreeBusyAccessEnabled $true -FreeBusyAccessLevel LimitedDetails
 
-
 >>> ONPREM
-
 Get-FederationInformation -DomainName Contoso.com | New-OrganizationRelationship -Name "Contoso" -FreeBusyAccessEnabled $true -FreeBusyAccessLevel LimitedDetails
 
 
-# LONG
+<Manually>
 
 >>> CLOUD
-
               $access = ‘LimitedDetails’
            $CloudName = "Fabrikam Cloud"
-        $Clouddomains = "otherTENANT.mail.onmicrosoft.com","otherTENANT.onmicrosoft.com"
+        $Clouddomains = "fabrikam.mail.onmicrosoft.com","fabrikam.onmicrosoft.com"
        $clouddiscover = "https://outlook.office365.com/autodiscover/autodiscover.svc" 
    $TargetAppURICloud = "outlook.com"
 
@@ -56,5 +51,5 @@ TargetAutodiscoverEpr = $LocalAutodiscover
 FreeBusyAccessEnabled = $true
   FreeBusyAccessLevel = $access
   FreeBusyAccessScope = $null
- TargetApplicationUri = $$TargetAppURIlocal
+ TargetApplicationUri = $TargetAppURIlocal
               Enabled = $true }
