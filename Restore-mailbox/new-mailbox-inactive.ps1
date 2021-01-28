532c68a1ec41f0679132d8aab1d9110d05a9794d
jahube <70081790+jahube@users.noreply.github.com>
2021-01-28 02:58:33 +0200

    Update new-mailbox-inactive.ps1

--- Restore-mailbox/new-mailbox-inactive.ps1
+++ Restore-mailbox/new-mailbox-inactive.ps1
@@ -1,37 +1,42 @@
 ﻿
-
-
-
 $user = "affected@user.com"
 
 $InactiveMailbox = Get-Mailbox -Identity $user -InactiveMailboxOnly
 
 $param = @{ InactiveMailbox = $InactiveMailbox.DistinguishedName
 
-Name = $InactiveMailbox.Name
+                       Name = $InactiveMailbox.Name
 
-FirstName = "FirstName"
+                  FirstName = "FirstName"
 
-LastName = "LastName"
+                   LastName = "LastName"
 
-DisplayName = $InactiveMailbox.DisplayName
+                DisplayName = $InactiveMailbox.DisplayName
 
-MicrosoftOnlineServicesID = $InactiveMailbox.MicrosoftOnlineServicesID
+  MicrosoftOnlineServicesID = $InactiveMailbox.MicrosoftOnlineServicesID
 
-Password = (ConvertTo-SecureString -String 'P@ssW0rd' -AsPlainText -Force)
+                   Password = (ConvertTo-SecureString -String 'P@ssW0rd' -AsPlainText -Force)
 
-ResetPasswordOnNextLogon = $false }
-
-New-Mailbox @param
+   ResetPasswordOnNextLogon = $false }
 
 
+                New-Mailbox @param
 
-###############
+
+########## reference #######################################################################################################
+# https://docs.microsoft.com/en-us/microsoft-365/compliance/recover-an-inactive-mailbox
+# https://docs.microsoft.com/en-us/exchange/troubleshoot/administration/reconnect-inactive-or-soft-deleted-mailboxes-to-ad
+############################################################################################################################
+
+
+
+######################
+# only for Lab repro #
+######################
 
 Install-Module MSOnline
 
 Connect-MsolService
- 
 
 Get-MsolUser -UserPrincipalName "affected@user.com" | Remove-MsolUser
 Get-MsolUser -UserPrincipalName "affected@user.com" -ReturnDeletedUsers | Remove-MsolUser -RemoveFromRecycleBin
