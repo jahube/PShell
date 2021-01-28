d355dcbfb80f54cd801d23d573d274b59b15a6cf
jahube <70081790+jahube@users.noreply.github.com>
2021-01-28 19:24:52 +0200

    Update HybridFBMesh.ps1

--- IntraorgConnector/HybridFBMesh.ps1
+++ IntraorgConnector/HybridFBMesh.ps1
@@ -1,33 +1,60 @@
+Run from All locations
 
-$access = ‘LimitedDetails’
+(1)      Cloud     (3)
 
-$CloudName = "Fabrikam Cloud"
+Contoso          Fabrikam
 
-$Clouddomains = "otherTENANT.mail.onmicrosoft.com","otherTENANT.onmicrosoft.com"
+(2)      Onprem    (4)
 
-$clouddiscover = "https://outlook.office365.com/autodiscover/autodiscover.svc" 
+#SHORT
+
+>>> CLOUD
+
+Get-FederationInformation -DomainName Contoso.onmicrosoft.com | New-OrganizationRelationship -Name "Contoso" -FreeBusyAccessEnabled $true -FreeBusyAccessLevel LimitedDetails
+
+
+>>> ONPREM
+
+Get-FederationInformation -DomainName Contoso.com | New-OrganizationRelationship -Name "Contoso" -FreeBusyAccessEnabled $true -FreeBusyAccessLevel LimitedDetails
+
+
+# LONG
+
+>>> CLOUD
+
+              $access = ‘LimitedDetails’
+           $CloudName = "Fabrikam Cloud"
+        $Clouddomains = "otherTENANT.mail.onmicrosoft.com","otherTENANT.onmicrosoft.com"
+       $clouddiscover = "https://outlook.office365.com/autodiscover/autodiscover.svc" 
+   $TargetAppURICloud = "outlook.com"
 
 $PARAM = @{      Name = $CloudName
           DomainNames = $Clouddomains
 TargetAutodiscoverEpr = $clouddiscover
-FreeBusyAccessEnabled = $true 
-  FreeBusyAccessLevel = $access 
-  FreeBusyAccessScope = $null 
+FreeBusyAccessEnabled = $true
+  FreeBusyAccessLevel = $access
+  FreeBusyAccessScope = $null
+ TargetApplicationUri = $TargetAppURICloud
               Enabled = $true }
 
 
-$access = ‘LimitedDetails’
+>>> ONPREM
 
-$LocalName = "Fabrikam Onprem"
+Get-FederationInformation -DomainName Contoso.com | New-OrganizationRelationship -Name "Contoso" -FreeBusyAccessEnabled $true -FreeBusyAccessLevel LimitedDetails
 
-$Localdomains = "fabrikam.com","other.fabrikam.com"
+OR 
 
-$LocalAutodiscover = "https://mail.fabrikam.com/autodiscover/autodiscover.svc"
+              $access = ‘LimitedDetails’
+           $LocalName = "Fabrikam Onprem"
+        $Localdomains = "fabrikam.com","other.fabrikam.com"
+   $LocalAutodiscover = "https://mail.fabrikam.com/autodiscover/autodiscover.svc"
+   $TargetAppURIlocal = "mail.fabrikam.com"
 
 $PARAM = @{      Name = $LocalName
           DomainNames = $Localdomains
 TargetAutodiscoverEpr = $LocalAutodiscover
-FreeBusyAccessEnabled = $true 
-  FreeBusyAccessLevel = $access 
-  FreeBusyAccessScope = $null 
+FreeBusyAccessEnabled = $true
+  FreeBusyAccessLevel = $access
+  FreeBusyAccessScope = $null
+ TargetApplicationUri = $$TargetAppURIlocal
               Enabled = $true }
