01419d030c9c05a4410214627b6531bc9c08ad6f
jahube <70081790+jahube@users.noreply.github.com>
2021-02-01 05:19:16 +0200

    Update deduplicate-email-items.ps1

--- Misc/deduplicate-email-items.ps1
+++ Misc/deduplicate-email-items.ps1
@@ -14,14 +14,13 @@ $Path = "$ENV:USERPROFILE\Downloads"
 
 ################ (2) Download + install + execute ################
 
-$Source = "https://raw.githubusercontent.com/michelderooij/Remove-DuplicateItems/master/Remove-DuplicateItems.ps1"
-
 Register-PackageSource -provider NuGet -name nugetRepository -location https://api.nuget.org/v3/index.json 
-
 Install-Package Exchange.WebServices.Managed.Api -ProviderName NuGet -source nugetRepository
-
 Import-module "$ENV:ProgramFiles\PackageManagement\NuGet\Packages\Exchange.WebServices.Managed.Api.2.2.1.2\lib\net35\Microsoft.Exchange.WebServices.dll"
 
+
+$Source = "https://raw.githubusercontent.com/michelderooij/Remove-DuplicateItems/master/Remove-DuplicateItems.ps1"
 wget -Uri $Source -OutFile "$Path\Remove-DuplicateItems.ps1" ; Set-ExecutionPolicy bypass ; CD /D $Path ;
+
 
 $Path\Remove-DuplicateItems.ps1 -Identity $user -Server outlook.office365.com -Credentials $Credentials 
