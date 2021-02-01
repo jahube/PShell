3039f3118ed38a5b5793cb55ff4d006c53ea875e
jahube <70081790+jahube@users.noreply.github.com>
2021-02-01 05:27:34 +0200

    Update SCAN-CONTACTS.ps1

--- Misc/SCAN-CONTACTS.ps1
+++ Misc/SCAN-CONTACTS.ps1
@@ -3,28 +3,18 @@
 
 $user = "affected@mailbox.com"
 $Credentials = Get-Credential $user
+$Path = "$ENV:USERPROFILE\Downloads"
 
-#OLD# Register-PackageSource -provider NuGet -name nugetRepository -location https://www.nuget.org/api/v2  # V2 seems deprecated use below
-# unRegister-PackageSource nugetRepository
+Register-PackageSource -provider NuGet -name nugetRepository -location https://api.nuget.org/v3/index.json 
+Install-Package Exchange.WebServices.Managed.Api -ProviderName NuGet -source nugetRepository
+Import-module "$ENV:ProgramFiles\PackageManagement\NuGet\Packages\Exchange.WebServices.Managed.Api.2.2.1.2\lib\net35\Microsoft.Exchange.WebServices.dll"
 
-Register-PackageSource -provider NuGet -name nugetRepository -location https://api.nuget.org/v3/index.json # <-- NEW V3
+$Source = "https://raw.githubusercontent.com/jahube/PShell/master/Misc/SCAN-CONTACTS1.ps1"
+wget -Uri $Source -OutFile "$Path\SCAN-CONTACTS1.ps1" ; Set-ExecutionPolicy bypass ; CD /D $Path ;
 
-get-package Exchange.WebServices.Managed.Api
+$Path\SCAN-CONTACTS1.ps1 -Identity $user -Credentials $Credentials -Mode Full
 
-Install-Package Exchange.WebServices.Managed.Api -ProviderName NuGet -source nugetRepository # -RequiredVersion 2.2.1.2
-
-cd "$ENV:ProgramFiles\PackageManagement\NuGet\Packages\Exchange.WebServices.Managed.Api.2.2.1.2\lib\net35"
-
-$EWSDLL = 'Microsoft.Exchange.WebServices.dll'
-import-module "$pwd\$EWSDLL"
-
-
-wget -Uri "https://raw.githubusercontent.com/jahube/PShell/master/Misc/SCAN-CONTACTS1.ps1" -OutFile ".\SCAN-CONTACTS1.ps1"
-
-Set-ExecutionPolicy bypass -Confirm:$false -Force
-
-.\SCAN-CONTACTS1.ps1 -Identity $user -Credentials $Credentials -Mode Full
-$path = mkdir "C:\TempContacts"
+$pathout = mkdir "C:\TempContacts" -force
 
 # $global:searchedEWScontacts = @()
 # $global:SearchedEWSemails = @()
@@ -33,8 +23,8 @@ $global:SearchedEWSemails | Ft Name,Address
 
 $Searchedemails = $global:SearchedEWSemails | Select-Object Name,Address -Unique
 
-$Searchedemails | Export-Csv $path\EmailsExported.CSV -NoTypeInformation
+$Searchedemails | Export-Csv $pathout\EmailsExported.CSV -NoTypeInformation
 
-$global:searchedEWScontacts |  Export-Csv $path\contactsExported.CSV -NoTypeInformation
+$global:searchedEWScontacts |  Export-Csv $pathout\contactsExported.CSV -NoTypeInformation
 
-$global:searchedEWScontacts | Select FileAs,DisplayName,GivenName,Initials,MiddleName,NickName,CompanyName | Export-Csv $path\contactsShortOverview.CSV -NoTypeInformation
+$global:searchedEWScontacts | Select FileAs,DisplayName,GivenName,Initials,MiddleName,NickName,CompanyName | Export-Csv $pathout\contactsShortOverview.CSV -NoTypeInformation
