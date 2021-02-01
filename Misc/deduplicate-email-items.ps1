e45569fbde65a2c3d58a4371c3cef25d1cd5b3fd
jahube <70081790+jahube@users.noreply.github.com>
2021-02-01 18:29:11 +0200

    Update deduplicate-email-items.ps1

--- Misc/deduplicate-email-items.ps1
+++ Misc/deduplicate-email-items.ps1
@@ -10,17 +10,25 @@ $user = "affected@user.com"
 
 $Credentials = Get-Credential   
 
-$Path = "$ENV:USERPROFILE\Downloads"
-
 ################ (2) Download + install + execute ################
 
-Register-PackageSource -provider NuGet -name nugetRepository -location https://api.nuget.org/v3/index.json 
-Install-Package Exchange.WebServices.Managed.Api -ProviderName NuGet -source nugetRepository
-Import-module "$ENV:ProgramFiles\PackageManagement\NuGet\Packages\Exchange.WebServices.Managed.Api.2.2.1.2\lib\net35\Microsoft.Exchange.WebServices.dll"
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Register-PSRepository -Default
+Install-PackageProvider -Name NuGet
+
+Unregister-PackageSource -Name nugetRepository
+Register-PackageSource -provider NuGet -name nugetRepository -location "https://api.nuget.org/v3/index.json"
+Install-Package Exchange.WebServices.Managed.Api -ProviderName NuGet -source nugetRepository
+
+$EWSDLLPath = "$ENV:ProgramFiles\PackageManagement\NuGet\Packages\Exchange.WebServices.Managed.Api.2.2.1.2\lib\net35"
+cd $EWSDLLPath ; $EWSDLL = "Microsoft.Exchange.WebServices.dll"
+Import-module $EWSDLLPath\$EWSDLL
 
 $Source = "https://raw.githubusercontent.com/michelderooij/Remove-DuplicateItems/master/Remove-DuplicateItems.ps1"
-wget -Uri $Source -OutFile "$Path\Remove-DuplicateItems.ps1" ; Set-ExecutionPolicy bypass ; CD /D $Path ;
+wget -Uri $Source -OutFile "$EWSDLLPath\Remove-DuplicateItems.ps1" ; Set-ExecutionPolicy bypass -force -Confirm:$false
 
+.\Remove-DuplicateItems.ps1 -Identity "$user" -Server outlook.office365.com -Credentials $Credentials
 
-$Path\Remove-DuplicateItems.ps1 -Identity $user -Server outlook.office365.com -Credentials $Credentials 
+#shared mailboxes
+.\Remove-DuplicateItems.ps1 -Identity "$user" -Server outlook.office365.com -Credentials $Credentials -impersonation
