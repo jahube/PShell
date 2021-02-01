4d8952b220e380f659258f28fe10fc11a9158adb
jahube <70081790+jahube@users.noreply.github.com>
2021-02-01 05:17:56 +0200

    Update deduplicate-email-items.ps1

--- Misc/deduplicate-email-items.ps1
+++ Misc/deduplicate-email-items.ps1
@@ -1,29 +1,27 @@
 # source https://gallery.technet.microsoft.com/office/Removing-Duplicate-Items-f706e1cc
 # Autor's page: https://eightwone.com/2013/06/21/removing-duplicate-items-from-a-mailbox/
 #########################################################################################
-# NOTE: for shared mailbox assign full access permission and put the accessing user in credentials + parameter -Impersonationb
+
+# NOTE: for shared mailbox use parameter -Impersonation + assign full access
+
+################### (1) Modify + Run Variables ###################
 
 $user = "affected@user.com"
 
-$Credentials = Get-Credential                 
+$Credentials = Get-Credential   
 
-#OLD# Register-PackageSource -provider NuGet -name nugetRepository -location https://www.nuget.org/api/v2  # V2 seems deprecated use below
-# unRegister-PackageSource nugetRepository
+$Path = "$ENV:USERPROFILE\Downloads"
 
-Register-PackageSource -provider NuGet -name nugetRepository -location https://api.nuget.org/v3/index.json # <-- NEW V3
+################ (2) Download + install + execute ################
 
-get-package Exchange.WebServices.Managed.Api
+$Source = "https://raw.githubusercontent.com/michelderooij/Remove-DuplicateItems/master/Remove-DuplicateItems.ps1"
 
-Install-Package Exchange.WebServices.Managed.Api -ProviderName NuGet -source nugetRepository # -RequiredVersion 2.2.1.2
+Register-PackageSource -provider NuGet -name nugetRepository -location https://api.nuget.org/v3/index.json 
 
-cd "$ENV:ProgramFiles\PackageManagement\NuGet\Packages\Exchange.WebServices.Managed.Api.2.2.1.2\lib\net35"
+Install-Package Exchange.WebServices.Managed.Api -ProviderName NuGet -source nugetRepository
 
-$EWSDLL = 'Microsoft.Exchange.WebServices.dll'
+Import-module "$ENV:ProgramFiles\PackageManagement\NuGet\Packages\Exchange.WebServices.Managed.Api.2.2.1.2\lib\net35\Microsoft.Exchange.WebServices.dll"
 
-import-module "$pwd\$EWSDLL"
+wget -Uri $Source -OutFile "$Path\Remove-DuplicateItems.ps1" ; Set-ExecutionPolicy bypass ; CD /D $Path ;
 
-wget -Uri "https://raw.githubusercontent.com/michelderooij/Remove-DuplicateItems/master/Remove-DuplicateItems.ps1" -OutFile ".\Remove-DuplicateItems.ps1"
-
-Set-ExecutionPolicy bypass
-
-.\Remove-DuplicateItems.ps1 -Identity $user -Server outlook.office365.com -Credentials $Credentials -
+$Path\Remove-DuplicateItems.ps1 -Identity $user -Server outlook.office365.com -Credentials $Credentials 
