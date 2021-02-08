ec1a03a98ff1c8ecd9138b5bbe8c04468ed3b0df
jahube <70081790+jahube@users.noreply.github.com>
2021-02-08 17:04:28 +0200

    Update Delete-RECOVERABLE-ITEMS.ps1

--- Delete-RECOVERABLE-ITEMS/Delete-RECOVERABLE-ITEMS.ps1
+++ Delete-RECOVERABLE-ITEMS/Delete-RECOVERABLE-ITEMS.ps1
@@ -30,8 +30,13 @@ Set-Mailbox $user –RemoveDelayReleaseHoldApplied
 Get-Mailbox $user -resultsize unlimited | Search-mailbox -SearchDumpsterOnly -TargetMailbox "Discovery Search Mailbox" -TargetFolder "$user" -DeleteContent -Force
 
  # DELETE DUMPSTER + no backup
+# assign permissions and reconnect Powershell !!
+Add-RoleGroupMember "Compliance Management" -Member admin@domain.com
+
+# delete items bigger than 8 MB *fast 1-10 minutes
 Search-Mailbox $user -SearchQuery size>8200000 -SearchDumpsterOnly –DeleteContent
 
+# delete all dumpster items * slow 30 Minutes <-> 6h
 Search-Mailbox $user -SearchDumpsterOnly -DeleteContent
 
 Get-Mailbox $user -resultsize unlimited | Search-mailbox -SearchDumpsterOnly -DeleteContent -Force
