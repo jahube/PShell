56e67853832d76bdea9a3926614c944ca7144f9d
jahube <70081790+jahube@users.noreply.github.com>
2021-02-08 18:08:08 +0200

    Update set-HostedOutboundSpamFilterPolicy.php

--- Spamfilter/set-HostedOutboundSpamFilterPolicy.php
+++ Spamfilter/set-HostedOutboundSpamFilterPolicy.php
@@ -1,1 +1,8 @@
 Get-HostedOutboundSpamFilterPolicy | Set-HostedOutboundSpamFilterPolicy -AutoForwardingMode on
+
+# the Default is applied only if a specific is not applied
+# if you scope on a user, only that policy will apply for the user
+
+NEW-HostedOutboundSpamFilterPolicy "AutoForward" -AutoForwardingMode on
+
+New-HostedOutboundSpamFilterRule -Name "AutoForward rule" -HostedOutboundSpamFilterPolicy "AutoForward" -FromMemberOf "AutoForward security group"
