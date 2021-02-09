ca53fbbe4bc2cbdaaa6c7fb6bf12c875fda33a9a
jahube <70081790+jahube@users.noreply.github.com>
2021-02-09 13:32:10 +0200

    Create room-short.ps1

---
+++ Rooms/room-short.ps1
@@ -0,0 +1,40 @@
+﻿
+# Version (1) WITHDelegates
+
+
+#### start ###
+$mailbox = “mailbox@domain.com”
+$resourcedelegates = "chris@contoso.com","michelle@contoso.com"
+
+$WITHDelegates = @{ identity = $mailbox; 
+AutomateProcessing = 'AutoUpdate';
+AllowConflicts = $false;
+AllRequestOutOfPolicy = $false;
+DeleteSubject = $false;
+AddOrganizerToSubject = $true;
+ForwardRequestsToDelegates = $true;
+AllRequestInPolicy = $true;
+AllBookInPolicy = $false;
+ResourceDelegates = $resourcedelegates; }
+set-calendarprocessing @WITHDelegates 
+#### end ###
+
+
+# Version (2) WITHOUTDelegates 
+
+#### start ###
+$mailbox = “mailbox@domain.com”
+
+$WITHOUTDelegates = @{ identity = $mailbox;
+AutomateProcessing = 'Autoaccept';
+AllowConflicts = $false;
+AllRequestOutOfPolicy = $false;
+DeleteSubject = $false;
+AddOrganizerToSubject = $true;
+ForwardRequestsToDelegates = $false;
+AllRequestInPolicy = $false;
+AllBookInPolicy = $true;
+ResourceDelegates = $Null;}
+
+set-calendarprocessing @WITHOUTDelegates
+#### end ###
