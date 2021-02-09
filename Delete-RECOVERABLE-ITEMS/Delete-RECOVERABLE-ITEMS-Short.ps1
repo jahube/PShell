169b514231e97921464e6e0a9ec017079ed9a896
jahube <70081790+jahube@users.noreply.github.com>
2021-02-09 12:35:21 +0200

    Create Delete-RECOVERABLE-ITEMS-Short.ps1

---
+++ Delete-RECOVERABLE-ITEMS/Delete-RECOVERABLE-ITEMS-Short.ps1
@@ -0,0 +1,13 @@
+﻿$user = "email@affected-user.com"
+
+Set-Mailbox $user -SingleItemRecoveryEnabled $false
+Set-Mailbox $user -ElcProcessingDisabled $true
+
+# assign permissions
+Add-RoleGroupMember "Compliance Management" -Member admin@domain.com
+
+# delete items bigger than 8 MB *fast 1-10 minutes 
+Search-Mailbox $user -SearchQuery size>8200000 -SearchDumpsterOnly –DeleteContent
+
+# delete all dumpster items * slow 30 Minutes <-> 6h
+Search-Mailbox $user -SearchDumpsterOnly -DeleteContent
