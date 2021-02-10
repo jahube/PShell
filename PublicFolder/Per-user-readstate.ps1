a0072cd17ff0252a270ee0cf3eaa3e7a319f3ca3
jahube <70081790+jahube@users.noreply.github.com>
2021-02-10 18:18:15 +0200

    Update Per-user-readstate.ps1

--- PublicFolder/Per-user-readstate.ps1
+++ PublicFolder/Per-user-readstate.ps1
@@ -6,6 +6,6 @@ $folderscope = '\'                                             # **MODIFY**
 $PFs = get-publicfolder $folderscope -Recurse -ResultSize unlimited -EA silentlycontinue ;
 [System.Collections.ArrayList]$PFE = ($PFs).entryid ; $count = $PFE.count
 for ($F = 0; $F -lt $PFE.count; $F++) { $A = "Setting <PerUserReadStateEnabled> - current Folder" ; $Prc= $(($F/$count)*100);
-Write-Progress -Activity $A -Id 2 -ParentId 1 -Status $PFE[$F] -PercentComplete $Prc -SecondsRemaining (($count-$F)*3);
+Write-Progress -Activity $A -Id 2 -ParentId 1 -Status $PFE[$F] -PercentComplete $Prc -SecondsRemaining ($count-$F);
 Set-PublicFolder -Identity $PFE[$F] -PerUserReadStateEnabled:$true -Confirm:$false }
 ################################################################
