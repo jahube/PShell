aeb6e80e97161dc41c63e67e2c6d4a64fd2e6217
jahube <70081790+jahube@users.noreply.github.com>
2021-02-11 22:54:58 +0200

    Update Remove-Org-Holds-for-a-Mailbox2.ps1

--- DELETE-Maibox-but-KEEP-User/Remove-Org-Holds-for-a-Mailbox2.ps1
+++ DELETE-Maibox-but-KEEP-User/Remove-Org-Holds-for-a-Mailbox2.ps1
@@ -25,20 +25,20 @@ $policies | fl *guid*
 $EXO = $policies | where { $_.TeamsPolicy -eq $false } ; 
 
 foreach ($EP in $EXO) {
+
                      Try { Set-RetentionCompliancePolicy -Identity $EP.Guid -AddExchangeLocationException $user -CF:$false -EA stop } 
                    catch { write-host $error[0]|fl*}
                      Try { Set-RetentionCompliancePolicy -identity $EP.Guid -RetryDistribution -CF:$false -EA stop } 
                     catch { write-host $error[0]|fl* }
          Get-RetentionCompliancePolicy -Identity $EP.Guid -DistributionDetail | FT Name,Type,ExchangeLocation*,DistributionStatus,Enabled,Mode
          Get-RetentionCompliancePolicy -Identity $EP.Guid -DistributionDetail | select -ExpandProperty ExchangeLocationException
+
                        }
 
 ################ end ####################
 
 ######### Teams policy exclusion #########
-
 $teams = $policies | where { $_.TeamsPolicy -EQ $TRUE } ; 
-
 foreach ($TP in $teams) {
                       Try { Set-TeamsRetentionCompliancePolicy -Identity $TP.Guid -AddTeamsChatLocationException $user  -CF:$false -EA stop } 
                     catch { write-host $error[0] | fl }
@@ -47,17 +47,23 @@ foreach ($TP in $teams) {
           Get-TeamsRetentionCompliancePolicy -Identity $TP.Guid -DistributionDetail | FT Name,Type,TeamsChatLocation*,DistributionStatus,Enabled,Mode
           Get-TeamsRetentionCompliancePolicy -Identity $TP.Guid -DistributionDetail | select -ExpandProperty TeamsChatLocationException 
                          }
-
 ################ end ####################
+
  
 ######### Hold policy exclusion #########
+
 $Holds = Get-HoldCompliancePolicy ;
+
 foreach ($H in $Holds) {
+
                        Try { Set-HoldCompliancePolicy -Identity $H.GUID -RemoveExchangeLocation $user -CF:$false -EA stop } 
                      catch { write-host $error[0] | fl }
+
                        Try { Set-HoldCompliancePolicy -Identity $H.GUID -RetryDistribution -CF:$false -EA stop } 
                      catch { write-host $error[0] }
+
                             Get-HoldCompliancePolicy -Identity $H.GUID -DistributionDetail | FT *location* 
+
                         }
 ################ end ####################
 
@@ -68,12 +74,14 @@ Get-ComplianceSearch
 $search = Get-ComplianceSearch #| where { $_.ExchangeLocation -match 'all'}
 
 foreach ($S in $search) {
+
                        Try { Set-ComplianceSearch -Identity $S.Identity -RemoveExchangeLocation $user -CF:$false -EA stop } 
                      catch { write-host $error[0] | fl }
+
                        Try { Set-ComplianceSearch -Identity $S.Identity -AddExchangeLocationExclusion $user -CF:$false -EA stop } 
                      catch { write-host $error[0] | fl }
-                       Try { Set-ComplianceSearch -Identity $S.Identity -RetryDistribution -CF:$false -EA stop } 
-                     catch { write-host $error[0] }
+
                              Get-ComplianceSearch -Identity $S.Identity | FT *location*
                         }
+
 ################ end ####################
