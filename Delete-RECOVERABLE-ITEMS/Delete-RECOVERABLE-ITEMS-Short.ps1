947273d142c82aec9b55b6031b1e08f4ea210b83
jahube <70081790+jahube@users.noreply.github.com>
2021-02-15 19:24:05 +0200

    Update Delete-RECOVERABLE-ITEMS-Short.ps1

--- Delete-RECOVERABLE-ITEMS/Delete-RECOVERABLE-ITEMS-Short.ps1
+++ Delete-RECOVERABLE-ITEMS/Delete-RECOVERABLE-ITEMS-Short.ps1
@@ -1,10 +1,29 @@
-﻿$user = "email@affected-user.com"
+﻿Set-ExecutionPolicy RemoteSigned
 
-Set-Mailbox $user -SingleItemRecoveryEnabled $false
-Set-Mailbox $user -ElcProcessingDisabled $true
+# install
+Install-Module -Name ExchangeOnlineManagement
+
+$user = "email@affected-user.com"
+
+$admin = "admin@domain.com"
+
+# connect
+Connect-ExchangeOnline -UserPrincipalName $admin -ShowProgress $true
 
 # assign permissions
-Add-RoleGroupMember "Compliance Management" -Member admin@domain.com
+New-ManagementRoleAssignment -Role "Mailbox Import Export" -User $ADMIN
+New-ManagementRoleAssignment -Role "Mailbox Search" -User $ADMIN
+Add-RoleGroupMember "Compliance Management" -Member $ADMIN
+
+# IMPORTANT - connect AGAIN to update permission - IMPORTANT <<<
+Connect-ExchangeOnline -UserPrincipalName $admin -ShowProgress $true
+
+# PERMISSIONS
+# Enable-OrganizationCustomization -confirm:$false # remove first "#" if you get the message that it is not enabled
+
+# IMPORTANT - below has ~ 2h replication time - IMPORTANT <<<
+Set-Mailbox $user -SingleItemRecoveryEnabled $false
+Set-Mailbox $user -ElcProcessingDisabled $true
 
 # delete items bigger than 8 MB *fast 1-10 minutes 
 Search-Mailbox $user -SearchQuery size>8200000 -SearchDumpsterOnly –DeleteContent
@@ -14,3 +33,4 @@ Search-Mailbox $user -SearchDumpsterOnly -DeleteContent
 
 # get RESULT
 Get-MailboxFolderStatistics $user -FolderScope RecoverableItems | FL Name,FolderAndSubfolderSize,ItemsInFolderAndSubfolders
+
