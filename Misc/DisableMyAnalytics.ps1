dde72fc8df0aee89188db4dd3ae7db65cd8aa4d2
jahube <70081790+jahube@users.noreply.github.com>
2021-02-22 11:28:32 +0200

    Update DisableMyAnalytics.ps1

--- Misc/DisableMyAnalytics.ps1
+++ Misc/DisableMyAnalytics.ps1
@@ -5,7 +5,26 @@ $OptOutUsers =  @()
 $OptInUsers = @()
 $failed = @()
 
-# disable for all Users
+# disable for all Users // Set-MyAnalyticsFeatureConfig
+
+Foreach ($M in $mbx.userprincipalname) {
+If((Get-MyAnalyticsFeatureConfig -Identity $M).PrivacyMode -eq 'Opt-Out') { 
+write-Host "User $($M) already opted out" -F green } else {
+Try { Set-MyAnalyticsFeatureConfig -Identity $M -PrivacyMode $privacymode -EA 'stop' ; 
+Write-host "[Mailbox] $M set to $($privacymode)" -F green } catch { 
+Write-host $error[0].Exception.Message -F Yellow ; $failed+= $M  }
+If((Get-MyAnalyticsFeatureConfig -Identity $M).PrivacyMode -eq 'Opt-Out') { 
+Write-host "[Mailbox] $M set to $($privacymode) confirmed" -F cyan ; 
+$OptOutUsers += $M } else { $OptInUsers += $M }}}
+
+# disable on Exchange OWA APP level
+
+$insights = get-app -organizationapp | where { $_.Displayname -match 'insights'}
+$insights | fl displayname,description,defaultstateforuser,enabled,appid
+$insights | % { set-app -organizationapp $_.Appid -Enabled $false }
+$insights | % { set-app -organizationapp $_.Appid -DefaultStateForUser 'Disabled' }
+
+# disable for all Users // Set-UserAnalyticsConfig DEPRECATED
 
 Foreach ($M in $mbx.userprincipalname) {
 If((Get-UserAnalyticsConfig -Identity $M).PrivacyMode -eq 'Opt-Out') { 
@@ -17,11 +36,3 @@ If((Get-UserAnalyticsConfig -Identity $M).PrivacyMode -eq 'Opt-Out') {
 Write-host "[Mailbox] $M set to $($privacymode) confirmed" -F cyan ; 
 $OptOutUsers += $M } else { $OptInUsers += $M }}}
 
-
-
-# disable on Exchange OWA APP level
-
-$insights = get-app -organizationapp | where { $_.Displayname -match 'insights'}
-$insights | fl displayname,description,defaultstateforuser,enabled,appid
-$insights | % { set-app -organizationapp $_.Appid -Enabled $false }
-$insights | % { set-app -organizationapp $_.Appid -DefaultStateForUser 'Disabled' }
