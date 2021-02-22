0a75fed1ee28721be90d27adb9282118c0d2e22c
jahube <70081790+jahube@users.noreply.github.com>
2021-02-22 11:35:44 +0200

    Update DisableMyAnalytics.ps1

--- Misc/DisableMyAnalytics.ps1
+++ Misc/DisableMyAnalytics.ps1
@@ -1,20 +1,19 @@
 ﻿$MBX = get-mailbox -ResultSize unlimited
 
-$privacymode = "Opt-Out"
-$OptOutUsers =  @()
-$OptInUsers = @()
-$failed = @()
+$privacymode = "Opt-Out" # "Opt-In"
+
+$OptOutUsers =  @() ; $OptInUsers = @() ; $failed = @()
 
 # disable for all Users // Set-MyAnalyticsFeatureConfig
 
 Foreach ($M in $mbx.userprincipalname) {
-If((Get-MyAnalyticsFeatureConfig -Identity $M).PrivacyMode -eq 'Opt-Out') { 
-write-Host "User $($M) already opted out" -F green } else {
+If((Get-MyAnalyticsFeatureConfig -Identity $M).PrivacyMode -eq $privacymode) { 
+write-Host "User $($M) already $privacymode" -F green } else {
 Try { Set-MyAnalyticsFeatureConfig -Identity $M -PrivacyMode $privacymode -EA 'stop' ; 
-Write-host "[Mailbox] $M set to $($privacymode)" -F green } catch { 
+Write-host "[Mailbox] $M set to $privacymode" -F green } catch { 
 Write-host $error[0].Exception.Message -F Yellow ; $failed+= $M  }
-If((Get-MyAnalyticsFeatureConfig -Identity $M).PrivacyMode -eq 'Opt-Out') { 
-Write-host "[Mailbox] $M set to $($privacymode) confirmed" -F cyan ; 
+If((Get-MyAnalyticsFeatureConfig -Identity $M).PrivacyMode -eq $privacymode) { 
+Write-host "[Mailbox] $M set to $privacymode confirmed" -F cyan ; 
 $OptOutUsers += $M } else { $OptInUsers += $M }}}
 
 # disable on Exchange OWA APP level
@@ -23,16 +22,4 @@ $insights = get-app -organizationapp | where { $_.Displayname -match 'insights'}
 $insights | fl displayname,description,defaultstateforuser,enabled,appid
 $insights | % { set-app -organizationapp $_.Appid -Enabled $false }
 $insights | % { set-app -organizationapp $_.Appid -DefaultStateForUser 'Disabled' }
-
-# disable for all Users // Set-UserAnalyticsConfig DEPRECATED
-
-Foreach ($M in $mbx.userprincipalname) {
-If((Get-UserAnalyticsConfig -Identity $M).PrivacyMode -eq 'Opt-Out') { 
-write-Host "User $($M) already opted out" -F green } else {
-Try { Set-UserAnalyticsConfig -Identity $M -PrivacyMode $privacymode -EA 'stop' ; 
-Write-host "[Mailbox] $M set to $($privacymode)" -F green } catch { 
-Write-host $error[0].Exception.Message -F Yellow ; $failed+= $M  }
-If((Get-UserAnalyticsConfig -Identity $M).PrivacyMode -eq 'Opt-Out') { 
-Write-host "[Mailbox] $M set to $($privacymode) confirmed" -F cyan ; 
-$OptOutUsers += $M } else { $OptInUsers += $M }}}
 
