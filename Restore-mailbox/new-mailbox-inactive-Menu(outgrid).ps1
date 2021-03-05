cab67df398f9669692532775a4493e482ec2938a
jahube <70081790+jahube@users.noreply.github.com>
2021-03-05 12:50:53 +0200

    Create new-mailbox-inactive-Menu(outgrid).ps1

---
+++ Restore-mailbox/new-mailbox-inactive-Menu(outgrid).ps1
@@ -0,0 +1,21 @@
+﻿  # $Mbx = get-mailbox -SoftDeletedMailbox
+$Mbx = get-mailbox -InactiveMailboxOnly
+$M = $Mbx | select Name,displayname,userprincipalname,primarysmtpaddress,MicrosoftOnlineServicesID,WindowsEmailAddress | Out-GridView -PassThru -title "Select Mailbox to Re-Connect"
+Write-host $M -F cyan
+$FirstName = read-host "Enter First Name"
+ $LastName = read-host "Enter Last Name"
+$param = @{ InactiveMailbox = $InactiveMailbox.DistinguishedName
+                       Name = $InactiveMailbox.Name
+                  FirstName = $FirstName
+                   LastName = $LastName
+                DisplayName = $InactiveMailbox.DisplayName
+  MicrosoftOnlineServicesID = $InactiveMailbox.MicrosoftOnlineServicesID
+                   Password = (ConvertTo-SecureString -String 'P@ssW0rd' -AsPlainText -Force)
+   ResetPasswordOnNextLogon = $false }
+
+                New-Mailbox @param
+
+########## reference #######################################################################################################
+# https://docs.microsoft.com/en-us/microsoft-365/compliance/recover-an-inactive-mailbox
+# https://docs.microsoft.com/en-us/exchange/troubleshoot/administration/reconnect-inactive-or-soft-deleted-mailboxes-to-ad
+############################################################################################################################
