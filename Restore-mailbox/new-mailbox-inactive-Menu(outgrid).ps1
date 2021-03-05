46f726cd132606b7541ac2fe16e8152b8b8fb944
jahube <70081790+jahube@users.noreply.github.com>
2021-03-05 13:26:55 +0200

    Update new-mailbox-inactive-Menu(outgrid).ps1

--- Restore-mailbox/new-mailbox-inactive-Menu(outgrid).ps1
+++ Restore-mailbox/new-mailbox-inactive-Menu(outgrid).ps1
@@ -3,9 +3,9 @@ $Mbx = get-mailbox -InactiveMailboxOnly | where {($_.ExternaldirectoryObjectId -
 $M = $Mbx | select Name,displayname,userprincipalname,primarysmtpaddress,MicrosoftOnlineServicesID,WindowsEmailAddress | Out-GridView -PassThru -title "Select Mailbox to Re-Connect"
 Write-host $M -F cyan
 
-If ($M.MicrosoftOnlineServicesID -ne "") { $WEA = $M.MicrosoftOnlineServicesID }
-  ElseIf ($M.WindowsEmailAddress -ne "") { $WEA = $M.WindowsEmailAddress }
-                                    Else { $WEA = $M.primarysmtpaddress }
+If ($M.MicrosoftOnlineServicesID -ne "") { $WEA = $M.MicrosoftOnlineServicesID ; write-host "MSOL-SID used for Windowslive [$WEA]" -F yellow }
+  ElseIf ($M.WindowsEmailAddress -ne "") { $WEA = $M.WindowsEmailAddress ; write-host "WindowsEmailAddress used for Windowslive [$WEA]" -F yellow }
+                                    Else { $WEA = $M.primarysmtpaddress ; write-host "PrimarySMTP used for Windowslive [$WEA]" -F yellow}
 $UPN = $M.userprincipalname
 $splitname = $UPN.Split('@')[0].Replace('.', ' ')
 # output = First Last
