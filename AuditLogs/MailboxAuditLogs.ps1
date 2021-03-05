95c3d662b6c943475592773b56be09d6ab6c52aa
jahube <70081790+jahube@users.noreply.github.com>
2021-03-05 11:41:31 +0200

    Update MailboxAuditLogs.ps1

--- AuditLogs/MailboxAuditLogs.ps1
+++ AuditLogs/MailboxAuditLogs.ps1
@@ -1,7 +1,15 @@
+# EXO PS: connection commands reference
+# connect
+$ADMIN = "admin @ domain"                                    # modify Admin UPN ("login email")
+$path = "C:\mailboxlogs.xml"                                 # modify Logs path
+$user = "affected @ user"                                    # modify affected user UPN
+
 start-transcript
 
-$path = "C:\mailboxlogs.xml"
-$user = "affected user address"
+install-module exchangeonlinemanagement                      # only first time
+connect-exchangeonline -userprincipalname $ADMIN             # connect #1
+Add-RoleGroupMember "Organization Management" -Member $ADMIN # permissions
+connect-exchangeonline -userprincipalname $ADMIN             # reconnnect to reflect permissions - IMPORTANT
 
 Search-MailboxAuditLog -Identity $user -ShowDetails -StartDate (get-date).AddDays(-90) -EndDate (get-date) | Export-Clixml $path
 
