a46ccedf5e0eca77a94cb4493b6375a97295baf1
jahube <70081790+jahube@users.noreply.github.com>
2021-03-26 12:21:24 +0200

    Update Export-O365Group-Member.ps1

--- GROUP/Export-O365Group-Member.ps1
+++ GROUP/Export-O365Group-Member.ps1
@@ -14,15 +14,21 @@ $Members = @()
 $ps = $path + '\short\' + $DL.PrimarySmtpAddress + '.csv'
 $pc = $path + '\complete\' + $DL.PrimarySmtpAddress + '.csv'
 
-$Groupmembers = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Member
+$Groupmembers = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Member -ResultSize unlimited
 $Groupmembers | Export-csv "$GroupDirPath\Groupmembers-$Groupdirname.CSV" -NoTypeInformation
 $Groupmembers | Export-Clixml "$GroupDirPath\Groupmembers-$Groupdirname.XML"
 $Groupmembers | FT > "$GroupDirPath\Groupmembers-$Groupdirname.txt"
 
-$GroupOwners = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Owner
+$GroupOwners = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Owner -ResultSize unlimited
 $GroupOwners | Export-csv "$GroupDirPath\GroupOwners-$Groupdirname.CSV" -NoTypeInformation
 $GroupOwners | Export-Clixml "$GroupDirPath\GroupOwners-$Groupdirname.XML"
 $GroupOwners | FT > "$GroupDirPath\GroupOwners-$Groupdirname.txt"
+
+$GroupOwners = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Subscriber -ResultSize unlimited
+$GroupOwners | Export-csv "$GroupDirPath\GroupSubscriber-$Groupdirname.CSV" -NoTypeInformation
+$GroupOwners | Export-Clixml "$GroupDirPath\GroupSubscriber-$Groupdirname.XML"
+$GroupOwners | FT > "$GroupDirPath\GroupSubscriber-$Groupdirname.txt"
+
 
 $C++
 
