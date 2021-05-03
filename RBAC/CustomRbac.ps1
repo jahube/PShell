def0a8d301b92e9c08a4d04599d89da679f84284
jahube <70081790+jahube@users.noreply.github.com>
2021-05-03 03:51:51 +0200

    Update CustomRbac.ps1

--- RBAC/CustomRbac.ps1
+++ RBAC/CustomRbac.ps1
@@ -1,22 +1,30 @@
+
+         $NEW_RBAC = "Custom Role Assignment Policy"
+             $Role = "MyBaseOptions"          # Parent to clone
+          $NewRole = "MyBaseOptions_COPY2" #Name of the Child Clone
+       $commandlet = "Set-MailboxJunkEmailConfiguration"
+$Remove_Parameters = "TrustedSendersAndDomains", "ContactsTrusted", "TrustedListsOnly", "BlockedSendersAndDomains"
+
 ##################################################################################################################
-        $NEW_RBAC = "Custom Role Assignment Policy"
-            $Role = "MyBaseOptions"          # Parent to clone
-         $NewRole = "MyBaseOptions_COPY2" #Name of the Child Clone
-      $commandlet = "Set-MailboxJunkEmailConfiguration"
-$Remove_Parameter = "TrustedSendersAndDomains"
-
-###########################################################################################################
-#  EXO can only "clone" Roles adding an automatic copy under a Parent
+#  EXO "clones" a Role under a Parent
 #   (1) inherits parent RoleEntries
-#   (2) those can be added/removed in the child only
+#   (2) modify RoleEntries under the inherited items
+##################################################################################################################
 
-                   New-ManagementRole -Name $NewRole -Description @(Get-ManagementRole $Role).Description -Parent $Role
-                   set-ManagementRoleEntry $("$NewRole" +"\" + "$commandlet") -Parameters $Remove_Parameter -RemoveParameter
-           $RBAC = Get-RoleAssignmentPolicy | where { $_.IsDefault -eq $true }; $RBAC.AssignedRoles.RemoveAt($RBAC.AssignedRoles.IndexOf($Role))
-                   New-RoleAssignmentPolicy -Name $NEW_RBAC -Description $RBAC.Description -Roles @($RBAC.AssignedRoles + $NewRole)
+New-ManagementRole -Name $NewRole -Description @(Get-ManagementRole $Role).Description -Parent $Role
 
-###########################################################################################################
-# assign Custom Role Assignment Policy
+Set-ManagementRoleEntry $("$NewRole" +"\" + "$commandlet") -Parameters $Remove_Parameters -RemoveParameter
+
+##################################################################################################################
+
+$RBAC = Get-RoleAssignmentPolicy | where { $_.IsDefault -eq $true };
+
+$RBAC.AssignedRoles.RemoveAt($RBAC.AssignedRoles.IndexOf($Role)) 
+
+New-RoleAssignmentPolicy -Name $NEW_RBAC -Description $RBAC.Description -Roles @($RBAC.AssignedRoles + $NewRole) 
+
+##################################################################################################################
+# assign Custom Role Assignment Policy #
 
 $MBXs = Get-Mailbox -ResultSize unlimited | select Use*e,pri*s,Disp*e,Ex*id | Out-GridView -T "Select Users to modify" -PassThru ; $MBXs
 
