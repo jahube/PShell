3492b2d613c924a28f89dc878eee757e19c58f68
jahube <70081790+jahube@users.noreply.github.com>
2021-05-17 22:11:08 +0200

    Update ConvertDL.ps1

--- GROUP/ConvertDL.ps1
+++ GROUP/ConvertDL.ps1
@@ -1,25 +1,20 @@
-#Modify the name please
-$DL = Get-DistributionGroup –Identity "Test DL2"
-$DL = Get-DistributionGroup -Filter { primarysmtpaddress -like "*Test*"}
 
-# "Menu"
+# $DL = Get-DistributionGroup –Identity "Test DL2"
+# $DL = Get-DistributionGroup -Filter { primarysmtpaddress -like "*Test*"}
+
+# "Menu" - pick DL to upgrade
 $DL = Get-DistributionGroup | select *name*,alias,pri*,GUID | Out-Gridview -Outputmode Single -T "Select Distributionlist to Upgrade"
 
-# documents
+# documents   #OR $Path "C:\Temp"
 $Path = ([Environment]::GetFolderPath('MyDocuments'))
-
-#$Path "C:\Temp"
 
 #DL export
 $DL | Export-Csv -Path "$Path\DL_$($DL.Name).csv" -NoTypeInformation -Encoding UTF8 -Force
 
 #check
-$DL
-
-# $DL = Import-Csv -Path "$Path\DL_$($DL.Name).csv" -Encoding UTF8
+$DL # $DL = Import-Csv -Path "$Path\DL_$($DL.Name).csv" -Encoding UTF8
 
 #MEMBERS export
-
 $Members = Get-DistributionGroupMember –Identity $DL.distinguishedname –ResultSize Unlimited
 $Members | Export-Csv -Path "$Path\exportDL_$($DL.Name).csv" -NoTypeInformation -Encoding UTF8
 $Members | select *name*,alias,pri*,GUID | Export-Csv -Path "$Path\exportDLshort.csv" -NoTypeInformation -Encoding UTF8 -Force
@@ -28,38 +23,21 @@ $Members | select *name*,alias,pri*,GUID | Export-Csv -Path "$Path\exportDLshort
 #delete DL after export
 Remove-DistributionGroup -identity $DL.DistinguishedName
 
+sleep 5
+
 #create new O365 Group with same Properties
 New-UnifiedGroup -name $DL.Name -Alias $DL.Alias -PrimarySmtpAddress $DL.PrimarySmtpAddress -DisplayName $DL.DisplayName
 Set-UnifiedGroup -Identity $DL.Name -DisplayName $DL.DisplayName -Alias $DL.Alias -RequireSenderAuthenticationEnabled $false -SubscriptionEnabled -AutoSubscribeNewMembers
+
 #ADD Members
-$mcount = 1
-$Memberscount = $Members.count
-Foreach ($Member in $Members) { 
-#Write-Progress -Activity "Adding Member" -Id 2 -ParentId 1 -Status "Member: $($Member.DisplayName)" -PercentComplete (($mcount/$Memberscount)*100)
-try { Add-UnifiedGroupLinks -Identity $DL.Name -LinkType member -Links $Member.distinguishedname -EA stop -CF:$false } 
-catch { Writehost $error[0] ; Add-UnifiedGroupLinks -Identity $DL.Name -LinkType member -Links $Member.userprincipalname -EA stop -CF:$false }
-$mcount++
-}
+for ($M = 0; $M -lt $Members.count;) {
+  try { Add-UnifiedGroupLinks -Identity $DL.Name -LinkType member -Links $Members[$M].DistinguishedName -EA stop -CF:$false }
+catch { Add-UnifiedGroupLinks -Identity $DL.Name -LinkType member -Links $Members[$M].userprincipalname -CF:$false } ;
+$M++ ; Write-Progress -Activity "Adding Member" -Id 2 -ParentId 1 -Status $Members[$M].DisplayName -PercentComplete (($M/$Members.count)*100) } # end
+
 
 #result
 get-UnifiedGroup -Identity $DL.Name
 (get-UnifiedGroupLinks -Identity $DL.Name -LinkType member).count
 # Finished
--------------------------------------------------------------
-#############################################################
-Below is just for reference to remove from exportDLremove.csv
-#############################################################
-REMOVE - ONLY FOR REFERENCE
-$Path = "C:\temp"
-$Members = Import-Csv -Path "$Path\exportDLremove.csv" -Encoding UTF8
-$rcount = 1
-Foreach ($Member in $Members) { 
-Write-Progress -Activity "Removing Members" -Id 2 -ParentId 1 -Status "Member: $($Member.DisplayName)" -PercentComplete (($rcount/$Memberscount)*100)
-try { remove-UnifiedGroupLinks -Identity $DL.Name -LinkType member -Links $Member.distinguishedname -Confirm:$false } catch { Writehost $error[0] }
-$rcount++
-}
-
-#result
-get-UnifiedGroup -Identity $DL.Name
-(get-UnifiedGroupLinks -Identity $DL.Name -LinkType member).count 
 
