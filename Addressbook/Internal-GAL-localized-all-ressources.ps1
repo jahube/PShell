9713f8c398ab329ff3f990572c350fe6f7584589
jahube <70081790+jahube@users.noreply.github.com>
2021-06-01 13:06:56 +0200

    Update Internal-GAL-localized-all-ressources.ps1

--- Addressbook/Internal-GAL-localized-all-ressources.ps1
+++ Addressbook/Internal-GAL-localized-all-ressources.ps1
@@ -24,6 +24,7 @@ connect-Exchangeonline -credential $credential                  # connect AGAIN 
      $RoomList_AL = "Roomlists"
         $Rooms_AL = "Rooms"
    $Ressources_AL = "Ressources"
+$RessourcesALL_AL = "All Ressources"
      $Offline_GAL = “Offline Addressbook”
      $default_GAL = “Global Adressbook”
 $AdressbookPolicy = "Adressbook Policy”
@@ -36,6 +37,7 @@ $AdressbookPolicy = "Adressbook Policy”
      $RoomList_AL = "Raumliste"
         $Rooms_AL = "Räume"
    $Ressources_AL = "Ausrüstung"
+$RessourcesALL_AL = "Alle Ressourcen"
      $Offline_GAL = “Offline Addressbuch”
      $default_GAL = “Globales Addressbuch”
 $AdressbookPolicy = "Adressbook Policy”
@@ -47,15 +49,16 @@ New-AddressList -Name $Groups_AL -RecipientFilter {(RecipientType -eq 'DynamicDi
 New-AddressList -Name $RoomList_AL  -RecipientFilter {(RecipientTypeDetails -eq 'RoomList')}
 New-AddressList -Name $Rooms_AL -RecipientFilter {(RecipientTypeDetails -eq 'RoomMailbox')}
 New-AddressList -Name $Ressources_AL -RecipientFilter {(RecipientTypeDetails -eq 'EquipmentMailbox')}
+New-AddressList -Name $RessourcesALL_AL -RecipientFilter {(RecipientTypeDetails -eq 'EquipmentMailbox') -or (RecipientTypeDetails -eq 'RoomMailbox')}
 
 # Create Offline Address Book
-New-OfflineAddressBook -Name $Offline_GAL -AddressLists $intern_AL,$extern_AL,$Groups_AL,$RoomList_AL,$Rooms_AL,$Ressources_AL
+New-OfflineAddressBook -Name $Offline_GAL -AddressLists $intern_AL,$extern_AL,$Groups_AL,$RoomList_AL,$Rooms_AL,$Ressources_AL,$RessourcesALL_AL
 
 # Create Global Address Book INTERNAL only
 New-GlobalAddressList  -Name $default_GAL -RecipientFilter "(RecipientType -eq 'UserMailbox') -or (RecipientType -eq 'Mailuser') -or (RecipientTypeDetails -eq 'SharedMailbox')"
 
 # Create Address Book Policy
-New-AddressBookPolicy -Name $AdressbookPolicy -AddressLists $intern_AL,$extern_AL,$Groups_AL,$RoomList_AL,$Rooms_AL,$Ressources_AL -RoomList "\$RoomList_AL" -OfflineAddressBook “\$Offline_GAL” -GlobalAddressList “\$default_GAL”
+New-AddressBookPolicy -Name $AdressbookPolicy -AddressLists $intern_AL,$extern_AL,$Groups_AL,$RoomList_AL,$Rooms_AL,$Ressources_AL,$RessourcesALL_AL -RoomList "\$RoomList_AL" -OfflineAddressBook “\$Offline_GAL” -GlobalAddressList “\$default_GAL”
 
 #check wih test user first
 Get-Mailbox TESTUSER | Set-Mailbox -AddressBookPolicy $AdressbookPolicy
