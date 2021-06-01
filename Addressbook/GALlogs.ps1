2af1d66b02809a8849d40ecbae212d726a649a36
jahube <70081790+jahube@users.noreply.github.com>
2021-06-01 13:02:23 +0200

    Update GALlogs.ps1

--- Addressbook/GALlogs.ps1
+++ Addressbook/GALlogs.ps1
@@ -1,13 +1,10 @@
-$VerbosePreference = 'Continue'
-$FormatEnumerationLimit=-1
-
 start-transcript -verbose
 
 Write-host "`n`nGet-AddressList" -F yellow
 Get-AddressList |FL
 
 Write-host "`n`nGet-GlobalAddressList" -F yellow
-Get-GlobalAddressList |FL
+Get-GlobalAddressList | FL
 
 Write-host "`n`nGet-OfflineAddressBook" -F yellow
 Get-OfflineAddressBook | FL 
@@ -19,16 +16,16 @@ Write-host "`n`nGet-mailbox" -F yellow
 Get-mailbox “identity of a mailbox that has the problem with Address Lists” | FL
  
 Write-host "`n`nGet-AddressList |FL name, recipientfilter" -F yellow
-Get-AddressList |FL name, recipientfilter
+Get-AddressList | FL name, recipientfilter
 
 $ALs = Get-AddressList
 Foreach ($AL in $ALs){
  
 Write-Host "`n`n $($AL.Name) LdapRecipientFilter `n"
-Get-Recipient -RecipientPreviewFilter (Get-AddressList $AL.Name).LdapRecipientFilter
+Get-Recipient -RecipientPreviewFilter (Get-AddressList $AL.Name).LdapRecipientFilter | FT -AutoSize
  
 Write-Host "`n`n $($AL.Name) RecipientFilter `n"
-Get-Recipient -RecipientPreviewFilter (Get-Addresslist $AL.Name).recipientfilter
+Get-Recipient -RecipientPreviewFilter (Get-Addresslist $AL.Name).recipientfilter | FT -AutoSize
 }
  
 stop-transcript
