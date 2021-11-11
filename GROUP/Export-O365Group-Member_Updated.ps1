692d59e7b9e71d64470045c9a1b7fea418a97dd9
PSScript <78456813+PSScript@users.noreply.github.com>
2021-11-11 16:42:00 +0100

    Create Export-O365Group-Member_Updated.ps1

---
+++ GROUP/Export-O365Group-Member_Updated.ps1
@@ -0,0 +1,74 @@
+﻿# desktop/MS-Logs+Timestamp
+$ts = Get-Date -Format yyyyMMdd_hhmmss
+$DesktopPath = ([Environment]::GetFolderPath('Desktop'))
+$logsPATH = mkdir "$DesktopPath\MS-Logs\Group_Export_$ts"
+
+Start-Transcript "$logsPATH\Group_Export_$ts.txt"
+$FormatEnumerationLimit = -1
+
+$Groups = Get-UnifiedGroup -ResultSize unlimited
+
+$count = $Groups.count ; $C = 0 ; $Members = @() ; [Array]$data = @()
+# $data = "" | select Type,DisplayName,ExternalDirectoryObjectId,Name,Alias,PrimarySMTP,HiddenGroupMembership,HiddenFromExchangeClients,HiddenFromAddressLists,Subscription,ReportToOriginator,ReportToManager,RequireSenderAuthentication,MBX_SMTP,MBX_Alias,MBX_ExchangeGuid
+
+foreach ($G in $Groups) {
+Write-Progress -Activity "[Group - NAME]  $($G.name)  - [Group - SMTP] $($G.PrimarySmtpAddress)" -Status "Group $($C) / $($count)" -PercentComplete (($C/$count)*100) -SecondsRemaining "$($count-$C)" ;
+
+  Try { $MBX = @() ; $MBX = Get-Mailbox $G.ExternalDirectoryObjectId -GroupMailbox -ErrorAction stop } 
+catch { $MBX = "" | select PrimarySMTPaddress,Alias,ExchangeGuid ; $MBX.PrimarySMTPaddress = "failed" ; $MBX.Alias = "failed" ; $MBX.ExchangeGuid = "failed"
+write-host $Error[0].Exception.message -F yellow }
+
+# $item = "" | select Type,DisplayName,ExternalDirectoryObjectId,Name,Alias,PrimarySMTP,HiddenGroupMembership,HiddenFromExchangeClients,HiddenFromAddressLists,Subscription,ReportToOriginator,ReportToManager,RequireSenderAuthentication,MBX_SMTP,MBX_Alias,MBX_ExchangeGuid
+
+$item = [PSCustomObject]@{ Type = "Group"
+                    DisplayName = $G.DisplayName
+      ExternalDirectoryObjectId = $G.ExternalDirectoryObjectId
+                           Name = $G.Name
+                          Alias = $G.Alias
+                    PrimarySMTP = $G.PrimarySMTPaddress
+          HiddenGroupMembership = $G.HiddenGroupMembershipEnabled
+      HiddenFromExchangeClients = $G.HiddenFromExchangeClientsEnabled
+         HiddenFromAddressLists = $G.HiddenFromAddressListsEnabled
+                   Subscription = $G.SubscriptionEnabled
+             ReportToOriginator = $G.ReportToOriginatorEnabled
+                ReportToManager = $G.ReportToManagerEnabled
+    RequireSenderAuthentication = $G.RequireSenderAuthenticationEnabled
+                       MBX_SMTP = $MBX.PrimarySMTPaddress
+                      MBX_Alias = $MBX.Alias
+               MBX_ExchangeGuid = $MBX.ExchangeGuid }
+
+$data += $item
+
+$Groupdirname = $($G.Alias).Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+$GroupDirPath = mkdir "$logsPATH\$Groupdirname"
+$item | FL > "$GroupDirPath\Group-short-$Groupdirname.txt"
+$G | FL > "$GroupDirPath\Group-Long-$Groupdirname.txt"
+
+#$ps = $logsPATH + '\short\' + $G.PrimarySmtpAddress + '.csv'
+#$pc = $logsPATH + '\complete\' + $G.PrimarySmtpAddress + '.csv'
+
+$Groupmembers = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Member -ResultSize unlimited
+$Groupmembers | Export-csv "$GroupDirPath\Groupmembers-$Groupdirname.CSV" -NoTypeInformation
+$Groupmembers | Export-Clixml "$GroupDirPath\Groupmembers-$Groupdirname.XML"
+$Groupmembers | FT > "$GroupDirPath\Groupmembers-$Groupdirname.txt"
+
+$GroupOwners = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Owner -ResultSize unlimited
+$GroupOwners | Export-csv "$GroupDirPath\GroupOwners-$Groupdirname.CSV" -NoTypeInformation
+$GroupOwners | Export-Clixml "$GroupDirPath\GroupOwners-$Groupdirname.XML"
+$GroupOwners | FT > "$GroupDirPath\GroupOwners-$Groupdirname.txt"
+
+$GroupSubscribers = Get-UnifiedGroupLinks -Identity $G.ExternalDirectoryObjectId -LinkType Subscriber -ResultSize unlimited
+$GroupSubscribers | Export-csv "$GroupDirPath\GroupSubscribers-$Groupdirname.CSV" -NoTypeInformation
+$GroupSubscribers | Export-Clixml "$GroupDirPath\GroupSubscribers-$Groupdirname.XML"
+$GroupSubscribers | FT > "$GroupDirPath\GroupSubscribers-$Groupdirname.txt"
+
+$C++
+
+}
+
+$Data | Export-csv "$logsPATH\Group-Overview.csv" -force -NoTypeInformation
+
+Stop-Transcript
+
+Compress-Archive -Path $logsPATH -DestinationPath "$DesktopPath\MS-Logs\Group_Export_$ts" -Force # Zip Logs
+Invoke-Item $DesktopPath\MS-Logs # open file manager
